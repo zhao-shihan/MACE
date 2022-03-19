c8ce9b1dc8ae052adf800004d351848adc822a84
zhaoshihan <zhao_shihan@foxmail.com>
2022-03-19 18:46:26 +0800

    update BranchSocket interface

--- Core/DataModel/Interface/BranchSocket.hxx
+++ Core/DataModel/Interface/BranchSocket.hxx
@@ -1,25 +1,18 @@
 #pragma once
 
 #include "DataModel/Global.hxx"
-#include "ObserverPtr.hxx"
 
-template<typename Object_t, typename Address_t>
+template<typename Type, bool UseReferenceInGetAndSet = true>
 class MACE::DataModel::Interface::BranchSocket {
 public:
-    BranchSocket(const char* branchName);
+    BranchSocket() = default;
     virtual ~BranchSocket() noexcept = default;
     BranchSocket(const BranchSocket&) = delete;
     BranchSocket& operator=(const BranchSocket&) = delete;
 
-    [[nodiscard]] const char* BranchName() const { return fBranchName; }
-    [[nodiscard]] virtual Object_t& Value() = 0;
-    [[nodiscard]] virtual Address_t Address() = 0;
+    [[nodiscard]] virtual std::conditional_t<UseReferenceInGetAndSet, const Type&, Type> GetValue() const = 0;
+    virtual void SetValue(std::conditional_t<UseReferenceInGetAndSet, const Type&, Type> value) = 0;
 
-    virtual void CreateBranch(TTree& tree) { tree.Branch(fBranchName, Address()); }
-    virtual void ConnectToBranch(TTree& tree) { tree.SetBranchAddress(fBranchName, Address()); }
-
-protected:
-    const char* const fBranchName;
+    virtual void CreateBranch(TTree& tree) = 0;
+    virtual void ConnectToBranch(TTree& tree) = 0;
 };
-
-#include "DataModel/Interface/BranchSocket.ixx"
