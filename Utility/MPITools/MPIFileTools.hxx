1305e295036b96a139ce790fc915169bae4b056f
zhaoshihan <zhao_shihan@foxmail.com>
2022-03-19 18:33:49 +0800

    doc MPIFileTools

--- Utility/MPITools/MPIFileTools.hxx
+++ Utility/MPITools/MPIFileTools.hxx
@@ -9,57 +9,57 @@
 #include "UtilityForwardDeclaration.hxx"
 #include "ObserverPtr.hxx"
 
-// Create directories and file paths to help managing files during mpi processing.
-//
-// For example:
-//
-// An executable xxx will create a single result file, which do something like this:
-//
-//   MPI::Init(...);
-//   ...
-//   MPIFileTools mpiFileTools("result", ".root");
-//   SomeFileHandler::Open(mpiFileTools.GetFilePath());
-//   ... (do some work, could be MPI parallely)
-//   SomeFileHandler::Close();
-//   mpiFileTools.MergeRootFiles()
-//
-// When running xxx on 16 nodes (e.g. named as node0, ..., node15), each node has 24 processes :
-// A folder named result will be created, under which a series of folders named
-// node0, node1, ..., node15 will be created. And there will be a series of files named
-// result_rank0.root, ..., result_rank23.root under result/node0, etc.
-// Directory structure as:
-//
-// result ┬ node0 ┬ result_rank0.root
-//        │       ├ result_rank1.root
-//        │       ├ ...
-//        │       └ result_rank23.root
-//        ├ node1 ┬ result_rank24.root
-//        │       ├ result_rank25.root
-//        │       ├ ...
-//        │       └ result_rank47.root
-//        ├ ...
-//        └ node15 ┬ result_rank360.root
-//                 ├ result_rank361.root
-//                 ├ ...
-//                 └ result_rank383.root
-// xxx
-// result.root                                      -> (created by MergeRootFiles())
-//
-// When running xxx on 1 node (e.g. PC), which has 8 processes :
-// A folder named result will be created, under which a series of files named
-// result_rank0.root, ..., result_rank7.root will be created.
-// Directory structure as:
-//
-// result ┬ result_rank0.root
-//        ├ result_rank1.root
-//        ├ ...
-//        └ result_rank7.root
-// xxx
-// result.root                                      -> (created by MergeRootFiles())
-//
-// When just ./xxx (not in MPI mode) : 
-// Just a single result.root will be created.
-//
+/// Create directories and file paths to help managing files during mpi processing.
+///
+/// For example:
+///
+/// An executable xxx will create a single result file, which do something like this:
+///
+///   MPI::Init(...);
+///   ...
+///   MPIFileTools mpiFileTools("result", ".root");
+///   SomeFileHandler::Open(mpiFileTools.GetFilePath());
+///   ... (do some work, could be MPI parallely)
+///   SomeFileHandler::Close();
+///   mpiFileTools.MergeRootFiles()
+///
+/// When running xxx on 16 nodes (e.g. named as node0, ..., node15), each node has 24 processes :
+/// A folder named result will be created, under which a series of folders named
+/// node0, node1, ..., node15 will be created. And there will be a series of files named
+/// result_rank0.root, ..., result_rank23.root under result/node0, etc.
+/// Directory structure as:
+///
+/// result ┬ node0 ┬ result_rank0.root
+///        │       ├ result_rank1.root
+///        │       ├ ...
+///        │       └ result_rank23.root
+///        ├ node1 ┬ result_rank24.root
+///        │       ├ result_rank25.root
+///        │       ├ ...
+///        │       └ result_rank47.root
+///        ├ ...
+///        └ node15 ┬ result_rank360.root
+///                 ├ result_rank361.root
+///                 ├ ...
+///                 └ result_rank383.root
+/// xxx
+/// result.root                                      -> (created by MergeRootFiles())
+///
+/// When running xxx on 1 node (e.g. PC), which has 8 processes :
+/// A folder named result will be created, under which a series of files named
+/// result_rank0.root, ..., result_rank7.root will be created.
+/// Directory structure as:
+///
+/// result ┬ result_rank0.root
+///        ├ result_rank1.root
+///        ├ ...
+///        └ result_rank7.root
+/// xxx
+/// result.root                                      -> (created by MergeRootFiles())
+///
+/// When just ./xxx (not in MPI mode) : 
+/// Just a single result.root will be created.
+///
 class MACE::MPIFileTools final {
 public:
     MPIFileTools(std::string_view basicName, std::string_view suffix, const MPI::Comm& comm = MPI::COMM_WORLD);
