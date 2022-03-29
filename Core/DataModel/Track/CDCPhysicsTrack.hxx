2892f2ef9fa1368d4e4822e763f2bd5b2881b881
zhaoshihan <zhao_shihan@foxmail.com>
2022-03-30 02:30:51 +0800

    SimTracks

---
+++ Core/DataModel/Track/CDCPhysicsTrack.hxx
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "Core/DataModel/BranchSocket/FundamentalBranchSocket.hxx"
+#include "Core/DataModel/BranchSocket/ShortStringBranchSocket.hxx"
+#include "Core/DataModel/BranchSocket/VectorBranchSocket.hxx"
+#include "Core/DataModel/Track/ICDCTrack.hxx"
+#include "Utility/LiteralUnit.hxx"
+#include "Utility/PhysicalConstant.hxx"
+
+namespace MACE::Core::DataModel::Track {
+
+using BranchSocket::FloatBranchSocket;
+using BranchSocket::ShortStringBranchSocket;
+using BranchSocket::Vector3FBranchSocket;
+using Utility::ShortString;
+using Utility::PhysicalConstant::electron_mass_c2;
+using namespace Utility::LiteralUnit::MagneticFluxDensity;
+
+class CDCHelixTrack;
+
+class CDCPhysicsTrack : public ICDCTrack {
+public:
+    CDCPhysicsTrack() noexcept;
+    CDCPhysicsTrack(const CDCPhysicsTrack&) noexcept = default;
+    CDCPhysicsTrack(CDCPhysicsTrack&&) noexcept = default;
+    virtual ~CDCPhysicsTrack() noexcept = default;
+    CDCPhysicsTrack& operator=(const CDCPhysicsTrack&) noexcept = default;
+    CDCPhysicsTrack& operator=(CDCPhysicsTrack&&) noexcept = default;
+
+    explicit CDCPhysicsTrack(const CDCHelixTrack& helix, Double_t phiVertex = 0, Double_t B = 0.1_T, Double_t mass = electron_mass_c2);
+
+    [[nodiscard]] const auto& GetVertexPosition() const { return fVertexPosition; }
+    [[nodiscard]] const auto& GetVertexEnergy() const { return fVertexEnergy; }
+    [[nodiscard]] const auto& GetVertexMomentum() const { return fVertexMomentum; }
+    [[nodiscard]] const auto& GetParticle() const { return fParticle; }
+
+    template<typename Vector3_t>
+    void SetVertexPosition(Vector3_t&& pos) { fVertexPosition = std::forward<Vector3_t>(pos); }
+    void SetVertexPosition(Double_t x, Double_t y, Double_t z) { fVertexPosition = {x, y, z}; }
+    void SetVertexEnergy(Double_t E) { fVertexEnergy = E; }
+    template<typename Vector3_t>
+    void SetVertexMomentum(Vector3_t&& mom) { fVertexMomentum = std::forward<Vector3_t>(mom); }
+    void SetVertexMomentum(Double_t pX, Double_t pY, Double_t pZ) { fVertexMomentum = {pX, pY, pZ}; }
+    template<typename String_t>
+    void SetParticle(String_t&& particleName) { fParticle = std::forward<String_t>(particleName); }
+
+    static consteval const char* BasicTreeName() noexcept { return "PhyTrk"; }
+    static void CreateBranches(TTree& tree);
+    static void ConnectToBranches(TTree& tree);
+    void FillBranchSockets() const noexcept;
+
+private:
+    Eigen::Vector3d fVertexPosition;
+    Double_t fVertexEnergy;
+    Eigen::Vector3d fVertexMomentum;
+    ShortString fParticle;
+
+    static Vector3FBranchSocket fgVertexPosition;
+    static FloatBranchSocket fgVertexEnergy;
+    static Vector3FBranchSocket fgVertexMomentum;
+    static ShortStringBranchSocket fgParticle;
+};
+
+} // namespace MACE::Core::DataModel::Track
---
+++ Core/DataModel/Track/ICDCTrack.cxx
@@ -0,0 +1,38 @@
+#include "Core/DataModel/Track/ICDCTrack.hxx"
+
+namespace MACE::Core::DataModel::Track {
+
+IntBranchSocket ICDCTrack::fgNumHits("numHits", 0);
+FloatBranchSocket ICDCTrack::fgChi2("chi2", 0);
+DoubleBranchSocket ICDCTrack::fgVertexTime("vtxTime", 0);
+
+ICDCTrack::ICDCTrack() noexcept :
+    ITransientData(),
+    fNumHits(fgNumHits.GetValue()),
+    fChi2(fgChi2.GetValue()),
+    fVertexTime(fgVertexTime.GetValue()) {}
+
+ICDCTrack::~ICDCTrack() noexcept = default;
+
+void ICDCTrack::CreateBranches(TTree& tree) {
+    ITransientData::CreateBranches(tree);
+    fgNumHits.CreateBranch(tree);
+    fgChi2.CreateBranch(tree);
+    fgVertexTime.CreateBranch(tree);
+}
+
+void ICDCTrack::ConnectToBranches(TTree& tree) {
+    ITransientData::ConnectToBranches(tree);
+    fgNumHits.ConnectToBranch(tree);
+    fgChi2.ConnectToBranch(tree);
+    fgVertexTime.ConnectToBranch(tree);
+}
+
+void ICDCTrack::FillBranchSockets() const noexcept {
+    ITransientData::FillBranchSockets();
+    fgNumHits.SetValue(fNumHits);
+    fgChi2.SetValue(fChi2);
+    fgVertexTime.SetValue(fVertexTime);
+}
+
+} // namespace MACE::Core::DataModel::Track
