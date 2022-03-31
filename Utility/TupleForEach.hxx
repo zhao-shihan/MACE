1bf3beb9bb09f6f3811eeeae5365e03bf39b62f5
zhaoshihan <zhao_shihan@foxmail.com>
2022-04-01 03:38:29 +0800

    A nice for_each for tuple: TupleForEach

---
+++ Utility/TupleForEach.hxx
@@ -0,0 +1,52 @@
+/// @brief Traverses a std::tuple or its derived class.
+/// The usage is similar to std::ranges::for_each, except that the arg is a tuple.
+/// @author Shi-Han Zhao
+
+#pragma once
+
+#include <utility>
+
+namespace MACE::Utility {
+
+template<template<typename... T> class Tuple_Tt, class Functor_t, typename... Element_t>
+requires std::derived_from<Tuple_Tt<Element_t...>, std::tuple<Element_t...>>
+class TupleForEachImpl {
+    using TupleSize = std::integral_constant<std::size_t, sizeof...(Element_t)>;
+    using Tuple_t = Tuple_Tt<Element_t...>;
+
+    template<typename TupleCVRef_t, std::size_t... Index>
+    requires std::same_as<std::remove_cvref_t<TupleCVRef_t>, Tuple_t>
+    constexpr void Impl(TupleCVRef_t&& tuple, Functor_t&& func, std::index_sequence<Index...>&&) const {
+        (func(std::get<Index>(std::forward<TupleCVRef_t>(tuple))), ...);
+    }
+
+public:
+    template<typename TupleCVRef_t>
+    requires std::same_as<std::remove_cvref_t<TupleCVRef_t>, Tuple_t>
+    constexpr void operator()(TupleCVRef_t&& tuple, Functor_t&& func) const {
+        Impl(std::forward<TupleCVRef_t>(tuple), std::forward<Functor_t>(func), std::make_index_sequence<TupleSize::value>());
+    }
+};
+
+/// @brief Traverses a std::tuple or its derived class.
+/// The usage is similar to std::ranges::for_each, except that a tuple is passed in.
+template<template<typename... T> class Tuple_Tt, class Functor_t, typename... Element_t>
+constexpr void TupleForEach(const Tuple_Tt<Element_t...>& tuple, Functor_t&& func) {
+    TupleForEachImpl<Tuple_Tt, Functor_t, Element_t...>()(tuple, std::forward<Functor_t>(func));
+}
+
+/// @brief Traverses a std::tuple or its derived class.
+/// The usage is similar to std::ranges::for_each, except that a tuple is passed in.
+template<template<typename... T> class Tuple_Tt, class Functor_t, typename... Element_t>
+constexpr void TupleForEach(Tuple_Tt<Element_t...>& tuple, Functor_t&& func) {
+    TupleForEachImpl<Tuple_Tt, Functor_t, Element_t...>()(tuple, std::forward<Functor_t>(func));
+}
+
+/// @brief Traverses a std::tuple or its derived class.
+/// The usage is similar to std::ranges::for_each, except that a tuple is passed in.
+template<template<typename... T> class Tuple_Tt, class Functor_t, typename... Element_t>
+constexpr void TupleForEach(Tuple_Tt<Element_t...>&& tuple, Functor_t&& func) {
+    TupleForEachImpl<Tuple_Tt, Functor_t, Element_t...>()(tuple, std::forward<Functor_t>(func));
+}
+
+} // namespace MACE::Utility
