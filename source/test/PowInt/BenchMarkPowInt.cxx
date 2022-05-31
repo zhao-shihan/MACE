fa6e4a9560b64bbc01c7a8080420592b54d4e3a5
zhaoshihan <zhao_shihan@foxmail.com>
2022-05-31 12:24:32 +0800

    Fix BenchmarkPowInt fixed via ODR

--- source/test/PowInt/BenchMarkPowInt.cxx
+++ source/test/PowInt/BenchMarkPowInt.cxx
@@ -18,7 +18,7 @@ void BenchmarkPowInt(uint_fast32_t seed) {
         if constexpr (std::is_floating_point_v<T>) {
             return std::uniform_real_distribution<T>(-2, 2);
         } else if constexpr (std::is_integral_v<T>) {
-            return std::uniform_int_distribution<T>(1, 2);
+            return std::uniform_int_distribution<T>();
         }
     };
     auto Trans = GetTrans();
@@ -26,35 +26,62 @@ void BenchmarkPowInt(uint_fast32_t seed) {
     constexpr int warmUpCycle = 10000;
     constexpr int benchmarkCycle = 100000;
 
+    std::vector<T> dummy;
+    dummy.reserve(benchmarkCycle);
     steady_clock::time_point begin, end;
+
+    // background
     // warm up
     begin = steady_clock::now();
     for (int i = 0; i < warmUpCycle; ++i) {
-        PowInt<N>(Trans(randEng));
+        dummy.emplace_back(Trans(randEng));
     }
     end = steady_clock::now();
+    dummy.clear();
     // benchmark
     begin = steady_clock::now();
     for (int i = 0; i < benchmarkCycle; ++i) {
-        PowInt<N>(Trans(randEng));
+        dummy.emplace_back(Trans(randEng));
     }
     end = steady_clock::now();
+    dummy.clear();
+
+    std::cout << "Background: " << duration<double, std::milli>(end - begin).count() << " ms\t";
+
+    // benchmark PowInt
+    // warm up
+    begin = steady_clock::now();
+    for (int i = 0; i < warmUpCycle; ++i) {
+        dummy.emplace_back(PowInt<N>(Trans(randEng)));
+    }
+    end = steady_clock::now();
+    dummy.clear();
+    // benchmark
+    begin = steady_clock::now();
+    for (int i = 0; i < benchmarkCycle; ++i) {
+        dummy.emplace_back(PowInt<N>(Trans(randEng)));
+    }
+    end = steady_clock::now();
+    dummy.clear();
 
     const duration<double, std::milli> powIntTime = end - begin;
     std::cout << "PowInt<" << N << ">(x):   \t" << powIntTime.count() << " ms\t";
 
+    // benchmark std::pow
     // warm up
     begin = steady_clock::now();
     for (int i = 0; i < warmUpCycle; ++i) {
-        std::pow(Trans(randEng), N);
+        dummy.emplace_back(std::pow(Trans(randEng), N));
     }
     end = steady_clock::now();
+    dummy.clear();
     // benchmark
     begin = steady_clock::now();
     for (int i = 0; i < benchmarkCycle; ++i) {
-        std::pow(Trans(randEng), N);
+        dummy.emplace_back(std::pow(Trans(randEng), N));
     }
     end = steady_clock::now();
+    dummy.clear();
 
     const duration<double, std::milli> powTime = end - begin;
     std::cout << "std::pow(x, " << N << "): \t" << powTime.count() << " ms\tspeedup = " << powTime.count() / powIntTime.count() << std::endl;
@@ -70,14 +97,25 @@ int main(int argc, char* argv[]) {
     BenchmarkPowInt<Type, 1>(seed);                         \
     BenchmarkPowInt<Type, 2>(seed);                         \
     BenchmarkPowInt<Type, 3>(seed);                         \
+    BenchmarkPowInt<Type, 4>(seed);                         \
     BenchmarkPowInt<Type, 5>(seed);                         \
+    BenchmarkPowInt<Type, 6>(seed);                         \
+    BenchmarkPowInt<Type, 7>(seed);                         \
+    BenchmarkPowInt<Type, 8>(seed);                         \
+    BenchmarkPowInt<Type, 9>(seed);                         \
     BenchmarkPowInt<Type, 10>(seed);                        \
     BenchmarkPowInt<Type, 20>(seed);                        \
     BenchmarkPowInt<Type, 30>(seed);                        \
+    BenchmarkPowInt<Type, 40>(seed);                        \
     BenchmarkPowInt<Type, 50>(seed);                        \
+    BenchmarkPowInt<Type, 60>(seed);                        \
+    BenchmarkPowInt<Type, 70>(seed);                        \
+    BenchmarkPowInt<Type, 80>(seed);                        \
+    BenchmarkPowInt<Type, 90>(seed);                        \
     BenchmarkPowInt<Type, 100>(seed);                       \
     BenchmarkPowInt<Type, 200>(seed);                       \
     BenchmarkPowInt<Type, 300>(seed);                       \
+    BenchmarkPowInt<Type, 400>(seed);                       \
     BenchmarkPowInt<Type, 500>(seed)
 
 #define MACE_BENCHMARK_POWINT_FP(Type)                      \
@@ -86,15 +124,26 @@ int main(int argc, char* argv[]) {
     BenchmarkPowInt<Type, -1>(seed);                        \
     BenchmarkPowInt<Type, -2>(seed);                        \
     BenchmarkPowInt<Type, -3>(seed);                        \
+    BenchmarkPowInt<Type, -4>(seed);                        \
     BenchmarkPowInt<Type, -5>(seed);                        \
+    BenchmarkPowInt<Type, -6>(seed);                        \
+    BenchmarkPowInt<Type, -7>(seed);                        \
+    BenchmarkPowInt<Type, -8>(seed);                        \
+    BenchmarkPowInt<Type, -9>(seed);                        \
     BenchmarkPowInt<Type, -10>(seed);                       \
     BenchmarkPowInt<Type, -20>(seed);                       \
     BenchmarkPowInt<Type, -30>(seed);                       \
+    BenchmarkPowInt<Type, -40>(seed);                       \
     BenchmarkPowInt<Type, -50>(seed);                       \
+    BenchmarkPowInt<Type, -60>(seed);                       \
+    BenchmarkPowInt<Type, -70>(seed);                       \
+    BenchmarkPowInt<Type, -80>(seed);                       \
+    BenchmarkPowInt<Type, -90>(seed);                       \
     BenchmarkPowInt<Type, -100>(seed);                      \
     BenchmarkPowInt<Type, -200>(seed);                      \
     BenchmarkPowInt<Type, -300>(seed);                      \
-    BenchmarkPowInt<Type, -500>(seed);
+    BenchmarkPowInt<Type, -400>(seed);                      \
+    BenchmarkPowInt<Type, -500>(seed)
 
     MACE_BENCHMARK_POWINT_INT(char);
     MACE_BENCHMARK_POWINT_INT(unsigned char);
