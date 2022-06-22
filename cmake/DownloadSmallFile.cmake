d7edc3ed5f743a4f37b1ab609af0f10cf339cc76
zhaoshihan <zhao_shihan@foxmail.com>
2022-06-22 20:06:49 +0800

    Update DownloadSmallFile: request head b4 download

--- cmake/DownloadSmallFile.cmake
+++ cmake/DownloadSmallFile.cmake
@@ -16,12 +16,23 @@ function(mace_download_small_file SRC_URL DEST_PATH)
         if(NOT IS_DIRECTORY "${DEST_DIR}")
             message(FATAL_ERROR "The parent path of DEST_PATH (${DEST_DIR}) is not a directory.")
         endif()
-        # process curl
-        message(STATUS "Executing command: curl -o \"${DEST_PATH}\" \"${SRC_URL}\"")
-        execute_process(COMMAND "curl" "-o" "${DEST_PATH}" "${SRC_URL}"
+        # request head
+        message(STATUS "Executing command: curl -I -L -s -o \"${DEST_PATH}.head\" \"${SRC_URL}\"")
+        execute_process(COMMAND "curl" "-I" "-L" "-s" "-o" "${DEST_PATH}.head" "${SRC_URL}"
                         RESULT_VARIABLE CURL_RETURN)
         # check return
-        if(NOT ${CURL_RETURN} EQUAL 0)
+        if(${CURL_RETURN} EQUAL 0)
+            # rm head
+            file(REMOVE "${DEST_PATH}.head")
+            # download
+            message(STATUS "Executing command: curl -L -o \"${DEST_PATH}\" \"${SRC_URL}\"")
+            execute_process(COMMAND "curl" "-L" "-J" "-o" "${DEST_PATH}" "${SRC_URL}"
+                            RESULT_VARIABLE CURL_RETURN)
+            # check return
+            if(NOT ${CURL_RETURN} EQUAL 0)
+                message(SEND_ERROR "Failed to download from ${SRC_URL} to ${DEST_PATH}")
+            endif()
+        else()
             message(SEND_ERROR "Failed to download from ${SRC_URL} to ${DEST_PATH}")
         endif()
     endif()
