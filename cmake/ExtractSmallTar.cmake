6091d0923d9e2c57fd2d616352615b5da3e02ea4
zhaoshihan <zhao_shihan@foxmail.com>
2022-06-24 00:02:10 +0800

    Update cmake untar

--- cmake/ExtractSmallTar.cmake
+++ cmake/ExtractSmallTar.cmake
@@ -1,4 +1,4 @@
-function(mace_unpack_small_tar TAR_PATH DEST_DIR)
+function(mace_extract_small_tar TAR_PATH DEST_DIR)
     # check param
     if(NOT IS_ABSOLUTE "${TAR_PATH}")
         message(FATAL_ERROR "Parameter TAR_PATH needs to be an absolute path. (Current value: ${TAR_PATH})")
@@ -14,12 +14,15 @@ function(mace_unpack_small_tar TAR_PATH DEST_DIR)
     if(NOT IS_DIRECTORY "${DEST_DIR}")
         message(FATAL_ERROR "Parameter DEST_DIR needs to be a directory. (Current value: ${DEST_DIR})")
     endif()
-    # process tar -xf
-    message(STATUS "Executing command: tar -xf \"${TAR_PATH}\" -C \"${DEST_DIR}\"")
-    execute_process(COMMAND "tar" "-xf" "${TAR_PATH}" "-C" "${DEST_DIR}"
+    # process tar xf
+    message(STATUS "Extracting ${TAR_PATH} to ${DEST_DIR} (\"${CMAKE_COMMAND}\" -E tar xf \"${TAR_PATH}\")")
+    execute_process(COMMAND "${CMAKE_COMMAND}" -E tar xf "${TAR_PATH}"
+                    WORKING_DIRECTORY "${DEST_DIR}"
                     RESULT_VARIABLE TAR_RETURN)
     # check return
-    if(NOT ${TAR_RETURN} EQUAL 0)
-        message(SEND_ERROR "Failed to unpack ${TAR_PATH} to ${DEST_DIR}")
+    if(${TAR_RETURN} EQUAL 0)
+        message(STATUS "Extraction completed")
+    else()
+        message(SEND_ERROR "Failed to extract ${TAR_PATH} to ${DEST_DIR}, error: ${TAR_RETURN}")
     endif()
 endfunction()
