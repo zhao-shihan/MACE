4d7265185c14dd16c04edc3bf46d04d5da643fe3
zhaoshihan <zhao_shihan@foxmail.com>
2022-07-09 01:36:32 +0800

    Update LookForEigen.cmake

--- cmake/LookForEigen.cmake
+++ cmake/LookForEigen.cmake
@@ -19,20 +19,25 @@ if(MACE_BUILTIN_EIGEN)
     endif()
     # set download src and dest
     include(FetchContent)
-    set(MACE_BUILTIN_EIGEN_DIR "${MACE_PROJECT_3RDPARTY_DIR}/eigen-${MACE_BUILTIN_EIGEN_VERSION}")
+    set(MACE_BUILTIN_EIGEN_SRC_DIR "${MACE_PROJECT_3RDPARTY_DIR}/eigen-${MACE_BUILTIN_EIGEN_VERSION}")
     set(MACE_BUILTIN_EIGEN_URL "https://gitlab.com/libeigen/eigen/-/archive/${MACE_BUILTIN_EIGEN_VERSION}/eigen-${MACE_BUILTIN_EIGEN_VERSION}.tar.bz2")
-    if(EXISTS "${MACE_BUILTIN_EIGEN_DIR}/CMakeLists.txt")
-        message(STATUS "Reusing Eigen source ${MACE_BUILTIN_EIGEN_DIR}")
-        FetchContent_Declare(Eigen3 SOURCE_DIR "${MACE_BUILTIN_EIGEN_DIR}")
+    if(EXISTS "${MACE_BUILTIN_EIGEN_SRC_DIR}/CMakeLists.txt")
+        message(STATUS "Reusing Eigen source ${MACE_BUILTIN_EIGEN_SRC_DIR}")
+        FetchContent_Declare(Eigen3 SOURCE_DIR "${MACE_BUILTIN_EIGEN_SRC_DIR}")
     else()
-        message(STATUS "Fetching Eigen from ${MACE_BUILTIN_EIGEN_URL} to ${MACE_BUILTIN_EIGEN_DIR}")
-        FetchContent_Declare(Eigen3 SOURCE_DIR "${MACE_BUILTIN_EIGEN_DIR}"
+        message(STATUS "Eigen will be downloaded from ${MACE_BUILTIN_EIGEN_URL} to ${MACE_BUILTIN_EIGEN_SRC_DIR}")
+        FetchContent_Declare(Eigen3 SOURCE_DIR "${MACE_BUILTIN_EIGEN_SRC_DIR}"
                                     URL "${MACE_BUILTIN_EIGEN_URL}")
     endif()
     # configure it
-    message(STATUS ">>>>>>>> Configuring built-in Eigen (version: ${MACE_BUILTIN_EIGEN_VERSION})")
+    message(STATUS ">>>>>>>> Downloading (if required) and configuring built-in Eigen (version: ${MACE_BUILTIN_EIGEN_VERSION})")
     FetchContent_MakeAvailable(Eigen3)
-    message(STATUS "<<<<<<<< Configuring built-in Eigen (version: ${MACE_BUILTIN_EIGEN_VERSION}) - done")
+    message(STATUS "<<<<<<<< Downloading (if required) and configuring built-in Eigen (version: ${MACE_BUILTIN_EIGEN_VERSION}) - done")
+    # check download
+    if(NOT EXISTS "${MACE_BUILTIN_EIGEN_SRC_DIR}/CMakeLists.txt")
+        file(REMOVE_RECURSE "${CMAKE_BINARY_DIR}/_deps/eigen3-subbuild")
+        message(FATAL_ERROR "It seems that the download of Eigen is not successful. You can try to run cmake again, or manually download Eigen from ${MACE_BUILTIN_EIGEN_URL} and extract it to ${MACE_PROJECT_3RDPARTY_DIR} (and keep the directory structure). If the error persists, you can try to clean the build tree and restart the build.")
+    endif()
 endif()
 
 if(NOT MACE_BUILTIN_EIGEN)
