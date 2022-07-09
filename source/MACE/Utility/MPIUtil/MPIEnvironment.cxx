0ee91841633d3b1d4edb2869e332f898bf74cd50
zhaoshihan <zhao_shihan@foxmail.com>
2022-07-09 20:47:09 +0800

    Amend MPIEnviroment.cxx

--- source/MACE/Utility/MPIUtil/MPIEnvironment.cxx
+++ source/MACE/Utility/MPIUtil/MPIEnvironment.cxx
@@ -10,22 +10,34 @@ ObserverPtr<MPIEnvironment> MPIEnvironment::fgInstance = nullptr;
 
 MPIEnvironment::MPIEnvironment(int argc, char* argv[]) :
     fWorldCommRank(-1),
-    fWorldCommSize(-1) {
-
+    fWorldCommSize(-1),
+    fProcessorName() {
+    // Confirm MPI condition
     int mpiInitialized;
     MPI_Initialized(&mpiInitialized);
     if (mpiInitialized) {
         throw std::logic_error("MACE::Utility::MPIUtil::MPIEnvironment: MPI environment should only be initialized once.");
     }
-
-    fgInstance = this;
+    // Initialize MPI
     MPI_Init(&argc, &argv);
+    // Initialize fWorldCommRank
     MPI_Comm_rank(MPI_COMM_WORLD, &fWorldCommRank);
-    MPI_Comm_size(MPI_COMM_WORLD, &fWorldCommRank);
+    // Initialize fWorldCommSize
+    MPI_Comm_size(MPI_COMM_WORLD, &fWorldCommSize);
+    // Initialize fProcessorName
+    char processorName[MPI_MAX_PROCESSOR_NAME];
+    int processorNameLength;
+    MPI_Get_processor_name(processorName, &processorNameLength);
+    fProcessorName.assign(processorName, processorNameLength);
+    fProcessorName.shrink_to_fit();
+    // Export the instance
+    fgInstance = this;
 }
 
 MPIEnvironment::~MPIEnvironment() {
+    // Finalize MPI
     MPI_Finalize();
+    // Reset instance pointer
     fgInstance = nullptr;
 }
 
--- source/MACE/Utility/MPIUtil/MPIEnvironment.hxx
+++ source/MACE/Utility/MPIUtil/MPIEnvironment.hxx
@@ -2,21 +2,30 @@
 
 #include "MACE/Utility/ObserverPtr.hxx"
 
+#include <string>
+
 namespace MACE::Utility::MPIUtil {
 
-class MPIEnvironment {
+class MPIEnvironment final {
 public:
     MPIEnvironment(int argc, char* argv[]);
     ~MPIEnvironment();
     MPIEnvironment(const MPIEnvironment&) = delete;
     MPIEnvironment& operator=(const MPIEnvironment&) = delete;
 
-    static auto GetWorldCommRank() { return fgInstance->fWorldCommRank; }
-    static auto GetWorldCommSize() { return fgInstance->fWorldCommRank; }
+    static auto Available() { return fgInstance != nullptr; }
+    static const auto& WorldCommRank() { return fgInstance->fWorldCommRank; }
+    static const auto& WorldCommSize() { return fgInstance->fWorldCommSize; }
+    static const auto& ProcessorName() { return fgInstance->fProcessorName; }
+    static auto IsWorldMaster() { return WorldCommRank() == 0; }
+    static auto IsWorldWorker() { return WorldCommRank() > 0; }
+    static auto IsSerialized() { return WorldCommSize() == 1; }
+    static auto IsParallelized() { return WorldCommSize() > 1; }
 
 private:
     int fWorldCommRank;
     int fWorldCommSize;
+    std::string fProcessorName;
 
     static ObserverPtr<MPIEnvironment> fgInstance;
 };
