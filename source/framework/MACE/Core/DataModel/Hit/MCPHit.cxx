6b9a5bc4afe9325f1fa805ff5ab273f75ba69b41
zhaoshihan <zhao_shihan@foxmail.com>
2022-09-23 02:21:37 +0800

    Update MCPHit according to the concept

--- source/framework/MACE/Core/DataModel/Hit/MCPHit.cxx
+++ source/framework/MACE/Core/DataModel/Hit/MCPHit.cxx
@@ -2,35 +2,31 @@
 
 namespace MACE::Core::DataModel::Hit {
 
-DoubleBranchSocket MCPHit::fgHitTime("hitTime", 0);
-Vector2FBranchSocket MCPHit::fgHitPosition("hitPos", {"x", "y"}, {0, 0});
-Vector2FBranchSocket MCPHit::fgHitPositionVariance("hitPosVar", {"x", "y"}, {0, 0});
+BranchSocket::DoubleBranchSocket MCPHit::fgHitTime("hitTime", 0);
+BranchSocket::Vector2FBranchSocket MCPHit::fgHitPosition("hitPos", {"x", "y"}, {0, 0});
+BranchSocket::Vector2FBranchSocket MCPHit::fgHitPositionVariance("hitPosVar", {"x", "y"}, {0, 0});
 
 MCPHit::MCPHit() noexcept :
-    ITransientData(),
     fHitTime(fgHitTime.Value()),
     fHitPosition(fgHitPosition.Value<double>()),
     fHitPositionVariance(fgHitPositionVariance.Value<double>()) {}
 
+void MCPHit::FillBranchSockets() const noexcept {
+    fgHitTime.Value(fHitTime);
+    fgHitPosition.Value(fHitPosition);
+    fgHitPositionVariance.Value(fHitPositionVariance);
+}
+
 void MCPHit::CreateBranches(TTree& tree) {
-    ITransientData::CreateBranches(tree);
     fgHitTime.CreateBranch(tree);
     fgHitPosition.CreateBranch(tree);
     fgHitPositionVariance.CreateBranch(tree);
 }
 
 void MCPHit::ConnectToBranches(TTree& tree) {
-    ITransientData::ConnectToBranches(tree);
     fgHitTime.ConnectToBranch(tree);
     fgHitPosition.ConnectToBranch(tree);
     fgHitPositionVariance.ConnectToBranch(tree);
-}
-
-void MCPHit::FillBranchSockets() const noexcept {
-    Base::FillBranchSockets();
-    fgHitTime.Value(fHitTime);
-    fgHitPosition.Value(fHitPosition);
-    fgHitPositionVariance.Value(fHitPositionVariance);
 }
 
 } // namespace MACE::Core::DataModel::Hit
--- source/framework/MACE/Core/DataModel/Hit/MCPHit.hxx
+++ source/framework/MACE/Core/DataModel/Hit/MCPHit.hxx
@@ -3,49 +3,48 @@
 #include "MACE/Core/DataFactory.hxx"
 #include "MACE/Core/DataModel/BranchSocket/FundamentalBranchSocket.hxx"
 #include "MACE/Core/DataModel/BranchSocket/VectorBranchSocket.hxx"
-#include "MACE/Core/DataModel/ITransientData.hxx"
+#include "MACE/Core/DataModel/TransientData.hxx"
+#include "MACE/Utility/AssignVector.hxx"
+
+#include <utility>
+#include <string_view>
 
 namespace MACE::Core::DataModel::Hit {
 
-using BranchSocket::DoubleBranchSocket;
-using BranchSocket::Vector2FBranchSocket;
+using namespace std::string_view_literals;
 
-class MCPHit : public ITransientData {
-    using Base = ITransientData;
-
+class MCPHit {
 public:
     MCPHit() noexcept;
-    MCPHit(const MCPHit& hit) noexcept = default;
-    MCPHit(MCPHit&& hit) noexcept = default;
-    virtual ~MCPHit() noexcept = default;
-    MCPHit& operator=(const MCPHit& hit) noexcept = default;
-    MCPHit& operator=(MCPHit&& hit) noexcept = default;
+    virtual ~MCPHit() = default;
+
+    MCPHit(const MCPHit&) noexcept = default;
+    MCPHit(MCPHit&&) noexcept = default;
+    MCPHit& operator=(const MCPHit&) noexcept = default;
+    MCPHit& operator=(MCPHit&&) noexcept = default;
 
     const auto& HitTime() const { return fHitTime; }
     const auto& GetHitPosition() const { return fHitPosition; }
     const auto& GetHitPositionVariance() const { return fHitPositionVariance; }
 
-    void HitTime(Double_t val) { fHitTime = val; }
-    template<typename A2Vector>
-    void SetHitPosition(A2Vector&& pos) { fHitPosition = std::forward<A2Vector>(pos); }
-    void SetHitPosition(Double_t x, Double_t y) { fHitPosition = {x, y}; }
-    template<typename A2Vector>
-    void SetHitPositionVariance(A2Vector&& posVar) { fHitPositionVariance = std::forward<A2Vector>(posVar); }
-    void SetHitPositionVariance(Double_t xVar, Double_t yVar) { fHitPositionVariance = {xVar, yVar}; }
+    void HitTime(double val) { fHitTime = val; }
+    void SetHitPosition(auto&&... x) requires(sizeof...(x) > 0) { Utility::AssignVector2D(fHitPosition, std::forward<decltype(x)>(x)...); }
+    void SetHitPositionVariance(auto&&... s) requires(sizeof...(s) > 0) { Utility::AssignVector2D(fHitPositionVariance, std::forward<decltype(s)>(s)...); }
 
-    static consteval const char* BasicTreeName() noexcept { return "MCPHit"; }
+    void FillBranchSockets() const noexcept;
     static void CreateBranches(TTree& tree);
     static void ConnectToBranches(TTree& tree);
-    void FillBranchSockets() const noexcept;
+    static constexpr auto BasicTreeName() noexcept { return "MCPHit"sv; }
 
 private:
-    Double_t fHitTime;
+    double fHitTime;
     Eigen::Vector2d fHitPosition;
     Eigen::Vector2d fHitPositionVariance;
 
-    static DoubleBranchSocket fgHitTime;
-    static Vector2FBranchSocket fgHitPosition;
-    static Vector2FBranchSocket fgHitPositionVariance;
+    static BranchSocket::DoubleBranchSocket fgHitTime;
+    static BranchSocket::Vector2FBranchSocket fgHitPosition;
+    static BranchSocket::Vector2FBranchSocket fgHitPositionVariance;
 };
+static_assert(TransientData<MCPHit>);
 
 } // namespace MACE::Core::DataModel::Hit
