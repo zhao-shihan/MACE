0c8f7e87942cb93566de65f5d0db4b033cd78959
zhaoshihan <zhao_shihan@foxmail.com>
2022-09-23 02:22:29 +0800

    Update MCPSimHit according to the concept

--- source/framework/MACE/Core/DataModel/SimHit/MCPSimHit.cxx
+++ source/framework/MACE/Core/DataModel/SimHit/MCPSimHit.cxx
@@ -2,11 +2,11 @@
 
 namespace MACE::Core::DataModel::SimHit {
 
-DoubleBranchSocket MCPSimHit::fgVertexTime("vtxTime", 0);
-Vector3FBranchSocket MCPSimHit::fgVertexPosition("vtxPos", {"x", "y", "z"}, {0, 0, 0});
-ShortStringBranchSocket MCPSimHit::fgParticle("particle", "");
-IntBranchSocket MCPSimHit::fgG4EventID("g4EventID", -1);
-IntBranchSocket MCPSimHit::fgG4TrackID("g4TrackID", -1);
+BranchSocket::DoubleBranchSocket MCPSimHit::fgVertexTime("vtxTime", 0);
+BranchSocket::Vector3FBranchSocket MCPSimHit::fgVertexPosition("vtxPos", {"x", "y", "z"}, {0, 0, 0});
+BranchSocket::ShortStringBranchSocket MCPSimHit::fgParticle("particle", "");
+BranchSocket::IntBranchSocket MCPSimHit::fgG4EventID("g4EventID", -1);
+BranchSocket::IntBranchSocket MCPSimHit::fgG4TrackID("g4TrackID", -1);
 
 MCPSimHit::MCPSimHit() noexcept :
     MCPHit(),
@@ -15,6 +15,15 @@ MCPSimHit::MCPSimHit() noexcept :
     fParticle(fgParticle.Value()),
     fG4EventID(fgG4EventID.Value()),
     fG4TrackID(fgG4TrackID.Value()) {}
+
+void MCPSimHit::FillBranchSockets() const noexcept {
+    MCPHit::FillBranchSockets();
+    fgVertexTime.Value(fVertexTime);
+    fgVertexPosition.Value(fVertexPosition);
+    fgParticle.Value(fParticle);
+    fgG4EventID.Value(fG4EventID);
+    fgG4TrackID.Value(fG4TrackID);
+}
 
 void MCPSimHit::CreateBranches(TTree& tree) {
     MCPHit::CreateBranches(tree);
@@ -32,15 +41,6 @@ void MCPSimHit::ConnectToBranches(TTree& tree) {
     fgParticle.ConnectToBranch(tree);
     fgG4EventID.ConnectToBranch(tree);
     fgG4TrackID.ConnectToBranch(tree);
-}
-
-void MCPSimHit::FillBranchSockets() const noexcept {
-    MCPHit::FillBranchSockets();
-    fgVertexTime.Value(fVertexTime);
-    fgVertexPosition.Value(fVertexPosition);
-    fgParticle.Value(fParticle);
-    fgG4EventID.Value(fG4EventID);
-    fgG4TrackID.Value(fG4TrackID);
 }
 
 } // namespace MACE::Core::DataModel::SimHit
--- source/framework/MACE/Core/DataModel/SimHit/MCPSimHit.hxx
+++ source/framework/MACE/Core/DataModel/SimHit/MCPSimHit.hxx
@@ -3,14 +3,14 @@
 #include "MACE/Core/DataModel/BranchSocket/ShortStringBranchSocket.hxx"
 #include "MACE/Core/DataModel/BranchSocket/VectorBranchSocket.hxx"
 #include "MACE/Core/DataModel/Hit/MCPHit.hxx"
+#include "MACE/Utility/AssignVector.hxx"
+
+#include <string_view>
+#include <utility>
 
 namespace MACE::Core::DataModel::SimHit {
 
-using BranchSocket::DoubleBranchSocket;
-using BranchSocket::IntBranchSocket;
-using BranchSocket::ShortStringBranchSocket;
-using BranchSocket::Vector3FBranchSocket;
-using Utility::ShortString;
+using namespace std::string_view_literals;
 
 class MCPSimHit : public Hit::MCPHit {
 public:
@@ -27,32 +27,30 @@ public:
     const auto& GetG4EventID() const { return fG4EventID; }
     const auto& GetG4TrackID() const { return fG4TrackID; }
 
-    void SetVertexTime(Double_t val) { fVertexTime = val; }
-    template<typename A3Vector>
-    void SetVertexPosition(A3Vector&& pos) { fVertexPosition = std::forward<A3Vector>(pos); }
-    void SetVertexPosition(Double_t x, Double_t y, Double_t z) { fVertexPosition = {x, y, z}; }
-    template<typename AString>
-    void SetParticle(AString&& particleName) { fParticle = std::forward<AString>(particleName); }
-    void SetG4EventID(Int_t val) { fG4EventID = val; }
-    void SetG4TrackID(Int_t val) { fG4TrackID = val; }
+    void SetVertexTime(double val) { fVertexTime = val; }
+    void SetVertexPosition(auto&&... x) requires(sizeof...(x) > 0) { Utility::AssignVector3D(fVertexPosition, std::forward<decltype(x)>(x)...); }
+    void SetParticle(auto&& p) { fParticle = std::forward<decltype(p)>(p); }
+    void SetG4EventID(int val) { fG4EventID = val; }
+    void SetG4TrackID(int val) { fG4TrackID = val; }
 
-    static consteval const char* BasicTreeName() noexcept { return "MCPSimHit"; }
+    void FillBranchSockets() const noexcept;
     static void CreateBranches(TTree& tree);
     static void ConnectToBranches(TTree& tree);
-    void FillBranchSockets() const noexcept;
+    static constexpr auto BasicTreeName() noexcept { return "MCPSimHit"sv; }
 
 private:
-    Double_t fVertexTime;
+    double fVertexTime;
     Eigen::Vector3d fVertexPosition;
-    ShortString fParticle;
-    Int_t fG4EventID;
-    Int_t fG4TrackID;
+    Utility::ShortString fParticle;
+    int fG4EventID;
+    int fG4TrackID;
 
-    static DoubleBranchSocket fgVertexTime;
-    static Vector3FBranchSocket fgVertexPosition;
-    static ShortStringBranchSocket fgParticle;
-    static IntBranchSocket fgG4EventID;
-    static IntBranchSocket fgG4TrackID;
+    static BranchSocket::DoubleBranchSocket fgVertexTime;
+    static BranchSocket::Vector3FBranchSocket fgVertexPosition;
+    static BranchSocket::ShortStringBranchSocket fgParticle;
+    static BranchSocket::IntBranchSocket fgG4EventID;
+    static BranchSocket::IntBranchSocket fgG4TrackID;
 };
+static_assert(TransientData<MCPSimHit>);
 
 } // namespace MACE::Core::DataModel::SimHit
