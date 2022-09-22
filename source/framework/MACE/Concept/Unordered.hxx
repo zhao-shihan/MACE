bed75612fa3a2ad973031371e51f48d6522e58e7
zhaoshihan <zhao_shihan@foxmail.com>
2022-09-23 01:58:13 +0800

    Add concept Unordered NonComparable

---
+++ source/framework/MACE/Concept/Unordered.hxx
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace MACE::Concept {
+
+template<typename T>
+concept Unordered = requires(T & v, const T& cv) {
+    requires not requires { v <=> cv; };
+    requires not requires { cv <=> cv; };
+    requires not requires { v < cv; };
+    requires not requires { cv < cv; };
+    requires not requires { v > cv; };
+    requires not requires { cv > cv; };
+    requires not requires { v <= cv; };
+    requires not requires { cv <= cv; };
+    requires not requires { v >= cv; };
+    requires not requires { cv >= cv; };
+};
+
+} // namespace MACE::Concept
