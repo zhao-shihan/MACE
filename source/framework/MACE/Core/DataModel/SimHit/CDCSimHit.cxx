f37c520da3708aa05d71526e91e0cd0e309738d2
zhaoshihan <zhao_shihan@foxmail.com>
2022-09-23 02:21:58 +0800

    Update CDCSimHit according to the concept

--- source/framework/MACE/Core/DataModel/SimHit/CDCSimHit.cxx
+++ source/framework/MACE/Core/DataModel/SimHit/CDCSimHit.cxx
@@ -2,15 +2,15 @@
 
 namespace MACE::Core::DataModel::SimHit {
 
-FloatBranchSocket CDCSimHit::fgEnergy("hitEne", 0);
-Vector3FBranchSocket CDCSimHit::fgMomentum("hitMom", {"x", "y", "z"}, {0, 0, 0});
-DoubleBranchSocket CDCSimHit::fgVertexTime("vtxTime", 0);
-Vector3FBranchSocket CDCSimHit::fgVertexPosition("vtxPos", {"x", "y", "z"}, {0, 0, 0});
-FloatBranchSocket CDCSimHit::fgVertexEnergy("vtxEne", 0);
-Vector3FBranchSocket CDCSimHit::fgVertexMomentum("vtxMom", {"x", "y", "z"}, {0, 0, 0});
-ShortStringBranchSocket CDCSimHit::fgParticle("particle", "");
-IntBranchSocket CDCSimHit::fgG4EventID("g4EventID", -1);
-IntBranchSocket CDCSimHit::fgG4TrackID("g4TrackID", -1);
+BranchSocket::FloatBranchSocket CDCSimHit::fgEnergy("hitEne", 0);
+BranchSocket::Vector3FBranchSocket CDCSimHit::fgMomentum("hitMom", {"x", "y", "z"}, {0, 0, 0});
+BranchSocket::DoubleBranchSocket CDCSimHit::fgVertexTime("vtxTime", 0);
+BranchSocket::Vector3FBranchSocket CDCSimHit::fgVertexPosition("vtxPos", {"x", "y", "z"}, {0, 0, 0});
+BranchSocket::FloatBranchSocket CDCSimHit::fgVertexEnergy("vtxEne", 0);
+BranchSocket::Vector3FBranchSocket CDCSimHit::fgVertexMomentum("vtxMom", {"x", "y", "z"}, {0, 0, 0});
+BranchSocket::ShortStringBranchSocket CDCSimHit::fgParticle("particle", "");
+BranchSocket::IntBranchSocket CDCSimHit::fgG4EventID("g4EventID", -1);
+BranchSocket::IntBranchSocket CDCSimHit::fgG4TrackID("g4TrackID", -1);
 
 CDCSimHit::CDCSimHit() noexcept :
     CDCHit(),
@@ -23,6 +23,19 @@ CDCSimHit::CDCSimHit() noexcept :
     fParticle(fgParticle.Value()),
     fG4EventID(fgG4EventID.Value()),
     fG4TrackID(fgG4TrackID.Value()) {}
+
+void CDCSimHit::FillBranchSockets() const noexcept {
+    CDCHit::FillBranchSockets();
+    fgEnergy.Value(fEnergy);
+    fgMomentum.Value(fMomentum);
+    fgVertexTime.Value(fVertexTime);
+    fgVertexPosition.Value(fVertexPosition);
+    fgVertexEnergy.Value(fVertexEnergy);
+    fgVertexMomentum.Value(fVertexMomentum);
+    fgParticle.Value(fParticle);
+    fgG4EventID.Value(fG4EventID);
+    fgG4TrackID.Value(fG4TrackID);
+}
 
 void CDCSimHit::CreateBranches(TTree& tree) {
     CDCHit::CreateBranches(tree);
@@ -48,19 +61,6 @@ void CDCSimHit::ConnectToBranches(TTree& tree) {
     fgParticle.ConnectToBranch(tree);
     fgG4EventID.ConnectToBranch(tree);
     fgG4TrackID.ConnectToBranch(tree);
-}
-
-void CDCSimHit::FillBranchSockets() const noexcept {
-    CDCHit::FillBranchSockets();
-    fgEnergy.Value(fEnergy);
-    fgMomentum.Value(fMomentum);
-    fgVertexTime.Value(fVertexTime);
-    fgVertexPosition.Value(fVertexPosition);
-    fgVertexEnergy.Value(fVertexEnergy);
-    fgVertexMomentum.Value(fVertexMomentum);
-    fgParticle.Value(fParticle);
-    fgG4EventID.Value(fG4EventID);
-    fgG4TrackID.Value(fG4TrackID);
 }
 
 } // namespace MACE::Core::DataModel::SimHit
--- source/framework/MACE/Core/DataModel/SimHit/CDCSimHit.hxx
+++ source/framework/MACE/Core/DataModel/SimHit/CDCSimHit.hxx
@@ -3,22 +3,22 @@
 #include "MACE/Core/DataModel/BranchSocket/ShortStringBranchSocket.hxx"
 #include "MACE/Core/DataModel/BranchSocket/VectorBranchSocket.hxx"
 #include "MACE/Core/DataModel/Hit/CDCHit.hxx"
+#include "MACE/Utility/AssignVector.hxx"
+
+#include <utility>
+#include <string_view>
 
 namespace MACE::Core::DataModel::SimHit {
 
-using BranchSocket::DoubleBranchSocket;
-using BranchSocket::FloatBranchSocket;
-using BranchSocket::IntBranchSocket;
-using BranchSocket::ShortStringBranchSocket;
-using BranchSocket::Vector3FBranchSocket;
-using Utility::ShortString;
+using namespace std::string_view_literals;
 
 class CDCSimHit : public Hit::CDCHit {
 public:
     CDCSimHit() noexcept;
+    virtual ~CDCSimHit() = default;
+
     CDCSimHit(const CDCSimHit& hit) noexcept = default;
     CDCSimHit(CDCSimHit&& hit) noexcept = default;
-    virtual ~CDCSimHit() noexcept = default;
     CDCSimHit& operator=(const CDCSimHit& hit) noexcept = default;
     CDCSimHit& operator=(CDCSimHit&& hit) noexcept = default;
 
@@ -32,48 +32,42 @@ public:
     const auto& GetG4EventID() const { return fG4EventID; }
     const auto& GetG4TrackID() const { return fG4TrackID; }
 
-    void SetEnergy(Double_t E) { fEnergy = E; }
-    template<typename A3Vector>
-    void SetMomentum(A3Vector&& mom) { fMomentum = std::forward<A3Vector>(mom); }
-    void SetMomentum(Double_t pX, Double_t pY, Double_t pZ) { fMomentum = {pX, pY, pZ}; }
-    void SetVertexTime(Double_t val) { fVertexTime = val; }
-    template<typename A3Vector>
-    void SetVertexPosition(A3Vector&& pos) { fVertexPosition = std::forward<A3Vector>(pos); }
-    void SetVertexPosition(Double_t x, Double_t y, Double_t z) { fVertexPosition = {x, y, z}; }
-    void SetVertexEnergy(Double_t E) { fVertexEnergy = E; }
-    template<typename A3Vector>
-    void SetVertexMomentum(A3Vector&& mom) { fVertexMomentum = std::forward<A3Vector>(mom); }
-    void SetVertexMomentum(Double_t pX, Double_t pY, Double_t pZ) { fVertexMomentum = {pX, pY, pZ}; }
-    template<typename AString>
-    void SetParticle(AString&& particleName) { fParticle = std::forward<AString>(particleName); }
-    void SetG4EventID(Int_t val) { fG4EventID = val; }
-    void SetG4TrackID(Int_t val) { fG4TrackID = val; }
+    void SetEnergy(double E) { fEnergy = E; }
+    void SetMomentum(auto&&... p) requires(sizeof...(p) > 0) { Utility::AssignVector3D(fMomentum, std::forward<decltype(p)>(p)...); }
+    void SetVertexTime(double val) { fVertexTime = val; }
+    void SetVertexPosition(auto&&... x) requires(sizeof...(x) > 0) { Utility::AssignVector3D(fVertexPosition, std::forward<decltype(x)>(x)...); }
+    void SetVertexEnergy(double E) { fVertexEnergy = E; }
+    void SetVertexMomentum(auto&&... p) requires(sizeof...(p) > 0) { Utility::AssignVector3D(fVertexMomentum, std::forward<decltype(p)>(p)...); }
+    void SetParticle(auto&& p) { fParticle = std::forward<decltype(p)>(p); }
+    void SetG4EventID(int val) { fG4EventID = val; }
+    void SetG4TrackID(int val) { fG4TrackID = val; }
 
-    static consteval const char* BasicTreeName() noexcept { return "CDCSimHit"; }
+    void FillBranchSockets() const noexcept;
     static void CreateBranches(TTree& tree);
     static void ConnectToBranches(TTree& tree);
-    void FillBranchSockets() const noexcept;
+    static constexpr auto BasicTreeName() noexcept { return "CDCSimHit"sv; }
 
 private:
-    Double_t fEnergy;
+    double fEnergy;
     Eigen::Vector3d fMomentum;
-    Double_t fVertexTime;
+    double fVertexTime;
     Eigen::Vector3d fVertexPosition;
-    Double_t fVertexEnergy;
+    double fVertexEnergy;
     Eigen::Vector3d fVertexMomentum;
-    ShortString fParticle;
-    Int_t fG4EventID;
-    Int_t fG4TrackID;
+    Utility::ShortString fParticle;
+    int fG4EventID;
+    int fG4TrackID;
 
-    static FloatBranchSocket fgEnergy;
-    static Vector3FBranchSocket fgMomentum;
-    static DoubleBranchSocket fgVertexTime;
-    static Vector3FBranchSocket fgVertexPosition;
-    static FloatBranchSocket fgVertexEnergy;
-    static Vector3FBranchSocket fgVertexMomentum;
-    static ShortStringBranchSocket fgParticle;
-    static IntBranchSocket fgG4EventID;
-    static IntBranchSocket fgG4TrackID;
+    static BranchSocket::FloatBranchSocket fgEnergy;
+    static BranchSocket::Vector3FBranchSocket fgMomentum;
+    static BranchSocket::DoubleBranchSocket fgVertexTime;
+    static BranchSocket::Vector3FBranchSocket fgVertexPosition;
+    static BranchSocket::FloatBranchSocket fgVertexEnergy;
+    static BranchSocket::Vector3FBranchSocket fgVertexMomentum;
+    static BranchSocket::ShortStringBranchSocket fgParticle;
+    static BranchSocket::IntBranchSocket fgG4EventID;
+    static BranchSocket::IntBranchSocket fgG4TrackID;
 };
+static_assert(TransientData<CDCSimHit>);
 
 } // namespace MACE::Core::DataModel::SimHit
