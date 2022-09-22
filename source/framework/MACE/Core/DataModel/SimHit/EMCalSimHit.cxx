31caf7c085197ff8641b6588942dd34e34d62730
zhaoshihan <zhao_shihan@foxmail.com>
2022-09-23 02:22:17 +0800

    Update EMCalSimHit according to the concept

--- source/framework/MACE/Core/DataModel/SimHit/EMCalSimHit.cxx
+++ source/framework/MACE/Core/DataModel/SimHit/EMCalSimHit.cxx
@@ -2,15 +2,22 @@
 
 namespace MACE::Core::DataModel::SimHit {
 
-ShortStringBranchSocket EMCalSimHit::fgParticle("particle", "");
-IntBranchSocket EMCalSimHit::fgG4EventID("g4EventID", -1);
-IntBranchSocket EMCalSimHit::fgG4TrackID("g4TrackID", -1);
+BranchSocket::ShortStringBranchSocket EMCalSimHit::fgParticle("particle", "");
+BranchSocket::IntBranchSocket EMCalSimHit::fgG4EventID("g4EventID", -1);
+BranchSocket::IntBranchSocket EMCalSimHit::fgG4TrackID("g4TrackID", -1);
 
 EMCalSimHit::EMCalSimHit() noexcept :
     EMCalHit(),
     fParticle(fgParticle.Value()),
     fG4EventID(fgG4EventID.Value()),
     fG4TrackID(fgG4TrackID.Value()) {}
+
+void EMCalSimHit::FillBranchSockets() const noexcept {
+    EMCalHit::FillBranchSockets();
+    fgParticle.Value(fParticle);
+    fgG4EventID.Value(fG4EventID);
+    fgG4TrackID.Value(fG4TrackID);
+}
 
 void EMCalSimHit::CreateBranches(TTree& tree) {
     EMCalHit::CreateBranches(tree);
@@ -24,13 +31,6 @@ void EMCalSimHit::ConnectToBranches(TTree& tree) {
     fgParticle.ConnectToBranch(tree);
     fgG4EventID.ConnectToBranch(tree);
     fgG4TrackID.ConnectToBranch(tree);
-}
-
-void EMCalSimHit::FillBranchSockets() const noexcept {
-    EMCalHit::FillBranchSockets();
-    fgParticle.Value(fParticle);
-    fgG4EventID.Value(fG4EventID);
-    fgG4TrackID.Value(fG4TrackID);
 }
 
 } // namespace MACE::Core::DataModel::SimHit
--- source/framework/MACE/Core/DataModel/SimHit/EMCalSimHit.hxx
+++ source/framework/MACE/Core/DataModel/SimHit/EMCalSimHit.hxx
@@ -3,18 +3,20 @@
 #include "MACE/Core/DataModel/BranchSocket/ShortStringBranchSocket.hxx"
 #include "MACE/Core/DataModel/Hit/EMCalHit.hxx"
 
+#include <utility>
+#include <string_view>
+
 namespace MACE::Core::DataModel::SimHit {
 
-using BranchSocket::IntBranchSocket;
-using BranchSocket::ShortStringBranchSocket;
-using Utility::ShortString;
+using namespace std::string_view_literals;
 
 class EMCalSimHit : public Hit::EMCalHit {
 public:
     EMCalSimHit() noexcept;
+    virtual ~EMCalSimHit() = default;
+
     EMCalSimHit(const EMCalSimHit& hit) noexcept = default;
     EMCalSimHit(EMCalSimHit&& hit) noexcept = default;
-    virtual ~EMCalSimHit() noexcept = default;
     EMCalSimHit& operator=(const EMCalSimHit& hit) noexcept = default;
     EMCalSimHit& operator=(EMCalSimHit&& hit) noexcept = default;
 
@@ -22,23 +24,24 @@ public:
     const auto& GetG4EventID() const { return fG4EventID; }
     const auto& GetG4TrackID() const { return fG4TrackID; }
 
-    void SetParticle(auto&& name) { fParticle = std::forward<decltype(name)>(name); }
-    void SetG4EventID(Int_t val) { fG4EventID = val; }
-    void SetG4TrackID(Int_t val) { fG4TrackID = val; }
+    void SetParticle(auto&& p) { fParticle = std::forward<decltype(p)>(p); }
+    void SetG4EventID(int val) { fG4EventID = val; }
+    void SetG4TrackID(int val) { fG4TrackID = val; }
 
-    static consteval const char* BasicTreeName() noexcept { return "CalSimHit"; }
+    void FillBranchSockets() const noexcept;
     static void CreateBranches(TTree& tree);
     static void ConnectToBranches(TTree& tree);
-    void FillBranchSockets() const noexcept;
+    static constexpr auto BasicTreeName() noexcept { return "CalSimHit"sv; }
 
 private:
-    ShortString fParticle;
-    Int_t fG4EventID;
-    Int_t fG4TrackID;
+    Utility::ShortString fParticle;
+    int fG4EventID;
+    int fG4TrackID;
 
-    static ShortStringBranchSocket fgParticle;
-    static IntBranchSocket fgG4EventID;
-    static IntBranchSocket fgG4TrackID;
+    static BranchSocket::ShortStringBranchSocket fgParticle;
+    static BranchSocket::IntBranchSocket fgG4EventID;
+    static BranchSocket::IntBranchSocket fgG4TrackID;
 };
+static_assert(TransientData<EMCalSimHit>);
 
 } // namespace MACE::Core::DataModel::SimHit
