e0ce472a74302cbcdcd4397a4e6c2a55292332bf
zhaoshihan <zhao_shihan@foxmail.com>
2022-09-23 14:18:38 +0800

    Update SimTarget MuoniumSimTrack accordingly

--- source/simulation/MACE/SimTarget/MuoniumTrack.hxx
+++ source/simulation/MACE/SimTarget/MuoniumTrack.hxx
@@ -7,22 +7,17 @@
 
 namespace MACE::SimTarget {
 
-class MuoniumTrack final : public Geant4X::UseG4Allocator<MuoniumTrack>,
-                           public Core::DataModel::SimTrack::MuoniumTrack {
-    using Base = Core::DataModel::SimTrack::MuoniumTrack;
-
+class MuoniumTrack final : public Core::DataModel::SimTrack::MuoniumSimTrack,
+                           public Geant4X::UseG4Allocator<MuoniumTrack> {
 public:
     MuoniumTrack() noexcept = default;
+    ~MuoniumTrack() = default;
+
     MuoniumTrack(const MuoniumTrack&) noexcept = default;
     MuoniumTrack(MuoniumTrack&&) noexcept = default;
-    ~MuoniumTrack() noexcept = default;
     MuoniumTrack& operator=(const MuoniumTrack&) noexcept = default;
     MuoniumTrack& operator=(MuoniumTrack&&) noexcept = default;
-
-    void SetVertexPosition(const G4ThreeVector& pos) { Base::SetVertexPosition(pos.x(), pos.y(), pos.z()); }
-    void SetVertexMomentum(const G4ThreeVector& mom) { Base::SetVertexMomentum(mom.x(), mom.y(), mom.z()); }
-    void SetDecayPosition(const G4ThreeVector& pos) { Base::SetDecayPosition(pos.x(), pos.y(), pos.z()); }
-    void SetDecayMomentum(const G4ThreeVector& mom) { Base::SetDecayMomentum(mom.x(), mom.y(), mom.z()); }
 };
+static_assert(Core::DataModel::TransientData<MuoniumTrack>);
 
 } // namespace MACE::SimTarget
