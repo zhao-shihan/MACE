d2dbcde071d6ef6e6b488bedad1e7fea959cbf95
zhaoshihan <zhao_shihan@foxmail.com>
2022-10-01 01:59:54 +0800

    Update MT1993732

--- source/framework/MACE/Math/Random/Generator/MT1993732.hxx
+++ source/framework/MACE/Math/Random/Generator/MT1993732.hxx
@@ -1,14 +1,15 @@
 #pragma once
 
-#include "MACE/Math/Random/UniformRandomBitGeneratorBase.hxx"
+#include "MACE/Concept/FundamentalType.hxx"
+#include "MACE/Math/Random/UniformPseudoRandomBitGeneratorBase.hxx"
 
 #include <random>
 
 namespace MACE::Math::Random::Generator {
 
-class MT1993732 final : public UniformRandomBitGeneratorBase<MT1993732, std::mt19937::result_type> {
+class MT1993732 final : public UniformPseudoRandomBitGeneratorBase<MT1993732, std::mt19937::result_type> {
 public:
-    MT1993732();
+    MT1993732() = default;
     MT1993732(ResultType seed);
 
     auto operator()() { return fMT(); }
@@ -16,6 +17,11 @@ public:
 
     static constexpr auto Min() { return std::mt19937::min(); }
     static constexpr auto Max() { return std::mt19937::max(); }
+
+    template<Concept::Character AChar>
+    friend auto& operator<<(std::basic_ostream<AChar>& os, const MT1993732& self) { return os << self.fMT; }
+    template<Concept::Character AChar>
+    friend auto& operator>>(std::basic_istream<AChar>& is, MT1993732& self) { return is >> self.fMT; }
 
 private:
     std::mt19937 fMT;
--- source/framework/MACE/Math/Random/Generator/MT1993732.inl
+++ source/framework/MACE/Math/Random/Generator/MT1993732.inl
@@ -1,9 +1,7 @@
 namespace MACE::Math::Random::Generator {
 
-MT1993732::MT1993732() :
-    fMT() {}
-
 MT1993732::MT1993732(MT1993732::ResultType seed) :
+    UniformPseudoRandomBitGeneratorBase(),
     fMT(seed) {}
 
 } // namespace MACE::Math::Random::Generator
