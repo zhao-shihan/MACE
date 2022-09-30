01ff3b5e3ff904dd31e0dcacc3618aceb5238ef4
zhaoshihan <zhao_shihan@foxmail.com>
2022-10-01 02:02:41 +0800

    Add CLHEP random adapter

---
+++ source/framework/MACE/CLHEPX/Random/RandomEngineBase.hxx
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "MACE/Math/Parity.hxx"
+#include "MACE/Math/Random/Distribution/Uniform.hxx"
+#include "MACE/Math/Random/UniformPseudoRandomBitGenerator.hxx"
+
+#include "CLHEP/Random/RandomEngine.h"
+
+#include <fstream>
+#include <limits>
+#include <string>
+#include <string_view>
+#include <typeinfo>
+
+namespace MACE::CLHEPX::Random {
+
+template<Math::Random::UniformPseudoRandomBitGenerator PRBG>
+class RandomEngineBase : public CLHEP::HepRandomEngine {
+public:
+    RandomEngineBase();
+    RandomEngineBase(long seed);
+
+protected:
+    ~RandomEngineBase() = default;
+
+public:
+    double flat() override final { return Math::Random::Distribution::Uniform<double>()(fPRBG); }
+    void flatArray(const int size, double* vect) override final;
+
+    void setSeed(long seed, int = 0) override final;
+    void setSeeds(const long* seeds, int = 0) override final;
+
+    void saveStatus(const char filename[]) const override final;
+    void restoreStatus(const char filename[]) override final;
+    void showStatus() const override final;
+
+    std::ostream& put(std::ostream& os) const override final;
+    std::istream& get(std::istream& is) override final;
+
+    operator double() override final { return Math::Random::Distribution::Uniform<double>()(fPRBG); }
+    operator float() override final { return Math::Random::Distribution::Uniform<float>()(fPRBG); }
+    operator unsigned int() override final { return fPRBG(); }
+
+private:
+    /// @brief Do not use.
+    std::istream& getState(std::istream& is) override final;
+
+private:
+    PRBG fPRBG;
+};
+
+} // namespace MACE::CLHEPX::Random
+
+#include "MACE/CLHEPX/Random/RandomEngineBase.inl"
