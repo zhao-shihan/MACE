64a53c0af7de72975ada5ce57895e79cf98e7e05
zhaoshihan <zhao_shihan@foxmail.com>
2022-10-01 01:53:27 +0800

    Update AssignVector correspondingly

--- source/framework/MACE/Utility/AssignVector.hxx
+++ source/framework/MACE/Utility/AssignVector.hxx
@@ -12,14 +12,14 @@ namespace MACE::Utility {
 
 /// @brief Assign something to a vector. If lhs = rhs is well-formed, assign
 /// with operator=, else assign element by element.
-/// @example AssignVector<double, 3>(u, v); AssignVector<double, 3>(u, x, y, z);
+/// e.g. AssignVector<double, 3>(u, v); AssignVector<double, 3>(u, x, y, z);
 /// @tparam T The value type of the LHS vector.
 /// @tparam N The dimension of the LHS vector
 /// @param lhs The vector at left-hand side.
 /// @param rhs Something at right hand side.
 /// @return If lhs = rhs is well-formed, returns lhs = rhs, else returns the
 /// lvalue reference to lhs.
-template<Concept::ArithmeticExcludeBool T, std::size_t N>
+template<Concept::Arithmetic T, std::size_t N>
 decltype(auto) AssignVector(Concept::NumericVector<T, N> auto& lhs, auto&& rhs) requires
     std::assignable_from<decltype(lhs), decltype(rhs)> {
     return lhs = rhs;
@@ -27,14 +27,14 @@ decltype(auto) AssignVector(Concept::NumericVector<T, N> auto& lhs, auto&& rhs) 
 
 /// @brief Assign something to a vector. If lhs = rhs is well-formed, assign
 /// with operator=, else assign element by element.
-/// @example AssignVector<double, 3>(u, v); AssignVector<double, 3>(u, x, y, z);
+/// e.g. AssignVector<double, 3>(u, v); AssignVector<double, 3>(u, x, y, z);
 /// @tparam T The value type of the LHS vector.
 /// @tparam N The dimension of the LHS vector
 /// @param lhs The vector at left-hand side.
 /// @param rhs Something at right hand side.
 /// @return If lhs = rhs is well-formed, returns lhs = rhs, else returns the
 /// lvalue reference to lhs.
-template<Concept::ArithmeticExcludeBool T, std::size_t N>
+template<Concept::Arithmetic T, std::size_t N>
 decltype(auto) AssignVector(Concept::NumericVector<T, N> auto& lhs, Concept::Subscriptable auto&& rhs) requires(
     not std::assignable_from<decltype(lhs), decltype(rhs)>) {
     for (std::size_t i = 0; i < N; ++i) {
@@ -45,14 +45,14 @@ decltype(auto) AssignVector(Concept::NumericVector<T, N> auto& lhs, Concept::Sub
 
 /// @brief Assign something to a vector. If lhs = rhs is well-formed, assign
 /// with operator=, else assign element by element.
-/// @example AssignVector<double, 3>(u, v); AssignVector<double, 3>(u, x, y, z);
+/// e.g. AssignVector<double, 3>(u, v); AssignVector<double, 3>(u, x, y, z);
 /// @tparam T The value type of the LHS vector.
 /// @tparam N The dimension of the LHS vector
 /// @param lhs The vector at left-hand side.
 /// @param rhs Something at right hand side.
 /// @return If lhs = rhs is well-formed, returns lhs = rhs, else returns the
 /// lvalue reference to lhs.
-template<Concept::ArithmeticExcludeBool T, std::size_t N>
+template<Concept::Arithmetic T, std::size_t N>
 decltype(auto) AssignVector(Concept::NumericVector<T, N> auto& lhs, std::same_as<T> auto... rhs) requires(
     sizeof...(rhs) == N) {
     return AssignVector<T, N>(lhs, std::array{rhs...});
