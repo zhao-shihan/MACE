8d0075b23c41903694f7ed9ebdc7ee97ab791c70
zhaoshihan <zhao_shihan@foxmail.com>
2022-10-01 01:58:39 +0800

    Update UniformRandomBitGeneratorBase

--- source/framework/MACE/Math/Random/UniformRandomBitGeneratorBase.hxx
+++ source/framework/MACE/Math/Random/UniformRandomBitGeneratorBase.hxx
@@ -2,13 +2,13 @@
 
 #include "MACE/Math/Random/UniformRandomBitGenerator.hxx"
 
-#include <cmath>
 #include <concepts>
 
 namespace MACE::Math::Random {
 
 /// @brief Well-formed derivation of this class fulfills
-/// UniformRandomBitGenerator (a C++ named requirements).
+/// concepts UniformRandomBitGenerator and STDUniformRandomBitGenerator
+/// (the C++ named requirements UniformRandomBitGenerator).
 /// @tparam ADerived The finally derived class.
 /// @tparam AResult The output type of derived URBG.
 template<class ADerived, std::unsigned_integral AResult>
@@ -22,10 +22,10 @@ protected:
     constexpr ~UniformRandomBitGeneratorBase() = default;
 
 public:
-    constexpr bool operator==(const UniformRandomBitGeneratorBase&) const = default;
-
     static constexpr auto min() { return ADerived::Min(); }
     static constexpr auto max() { return ADerived::Max(); }
+
+    constexpr bool operator==(const UniformRandomBitGeneratorBase&) const = default;
 };
 
 } // namespace MACE::Math::Random
