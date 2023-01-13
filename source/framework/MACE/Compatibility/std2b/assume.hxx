4cb26e3a4221eb083915f642569a95a4cda81b7f
zhaoshihan <zhao_shihan@foxmail.com>
2023-01-13 22:58:25 +0800

    std2b: assume

---
+++ source/framework/MACE/Compatibility/std2b/assume.hxx
@@ -0,0 +1,24 @@
+#pragma once
+
+#if not __has_cpp_attribute(assume) and \
+    not(defined __GNUC__ or defined __clang__ or defined _MSC_VER)
+#    include "MACE/Compatibility/std2b/unreachable.hxx"
+#endif
+
+namespace MACE::inline Compatibility::std2b {
+
+constexpr void assume(bool condition) noexcept {
+#if __has_cpp_attribute(assume)
+    [[assume(condition)]]; // C++2b
+#elif defined __GNUC__
+    if (not condition) { __builtin_unreachable(); }
+#elif defined __clang__
+    __builtin_assume(condition);
+#elif defined _MSC_VER
+    __assume(condition);
+#else
+    if (not condition) { unreachable(); }
+#endif
+}
+
+} // namespace MACE::inline Compatibility::std2b
