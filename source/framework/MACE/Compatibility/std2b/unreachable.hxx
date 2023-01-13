a80c9d64420a126fc09fb9b691e05c23b7dc3f1f
zhaoshihan <zhao_shihan@foxmail.com>
2023-01-13 22:58:54 +0800

    Update std2b::unreachable

--- source/framework/MACE/Compatibility/std2b/unreachable.hxx
+++ source/framework/MACE/Compatibility/std2b/unreachable.hxx
@@ -6,18 +6,18 @@
 #    include <utility>
 #endif
 
-namespace MACE::Compatibility::std2b {
+namespace MACE::inline Compatibility::std2b {
 
 [[noreturn]] inline void unreachable() {
-#ifdef __cpp_lib_unreachable // C++2b
-    std::unreachable();
-#else // fallback: compiler extensions
-#    ifdef __GNUC__
+#ifdef __cpp_lib_unreachable
+    std::unreachable(); // C++2b
+#elif defined __GNUC__ or defined __clang__
     __builtin_unreachable();
-#    elif defined _MSC_VER
+#elif defined _MSC_VER
     __assume(false);
-#    endif
+#else
+    return;
 #endif
 }
 
-} // namespace MACE::Compatibility::std2b
+} // namespace MACE::inline Compatibility::std2b
