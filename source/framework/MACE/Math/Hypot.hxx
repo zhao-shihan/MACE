d7db846c96fc7387c180246bf994532e480344df
zhaoshihan <zhao_shihan@foxmail.com>
2023-01-17 19:42:54 +0800

    Update Math/Hypot.hxx

--- source/framework/MACE/Math/Hypot.hxx
+++ source/framework/MACE/Math/Hypot.hxx
@@ -1,22 +1,27 @@
 #pragma once
 
-#include "MACE/Concept/FundamentalType.hxx"
-
 #include <cmath>
 #include <concepts>
+#include <type_traits>
 
 namespace MACE::Math {
 
-constexpr auto Hypot2(Concept::Arithmetic auto x, Concept::Arithmetic auto y) {
-    return x * x + y * y;
+constexpr auto Hypot2(std::floating_point auto... x) requires(sizeof...(x) >= 2) {
+    return (... + (x * x));
 }
 
-constexpr auto Hypot2(Concept::Arithmetic auto x, Concept::Arithmetic auto... y) {
-    return x * x + Hypot2(y...);
-}
-
-auto Hypot(std::floating_point auto... x) {
+auto Hypot(std::floating_point auto... x) requires(sizeof...(x) >= 2) {
     return std::sqrt(Hypot2(x...));
+}
+
+template<std::floating_point T = double>
+constexpr auto Hypot2(auto... x) requires(sizeof...(x) >= 2 and not(... and std::floating_point<decltype(x)>)) {
+    return Hypot2<std::common_type_t<T, decltype(x)...>>(x...);
+}
+
+template<std::floating_point T = double>
+auto Hypot(auto... x) requires(sizeof...(x) >= 2 and not(... and std::floating_point<decltype(x)>)) {
+    return Hypot<std::common_type_t<T, decltype(x)...>>(x...);
 }
 
 } // namespace MACE::Math
