8de055b88fbc03cfe12baa5c444cb5c2e0eb9ab7
zhaoshihan <zhao_shihan@foxmail.com>
2023-01-17 20:30:18 +0800

    Add diagnoal 3D Gaussian distribution

---
+++ source/framework/MACE/Math/Random/Distribution/Gaussian3DDiagnoal.hxx
@@ -0,0 +1,178 @@
+#pragma once
+
+#include "MACE/Concept/NumericVector.hxx"
+#include "MACE/Math/Random/Distribution/Gaussian.hxx"
+#include "MACE/Math/Random/RandomNumberDistributionBase.hxx"
+#include "MACE/Utility/ValueTypeOf.hxx"
+
+#include <array>
+#include <concepts>
+#include <iomanip>
+#include <type_traits>
+#include <utility>
+
+namespace MACE::Math::Random::Distribution {
+
+namespace internal {
+
+template<Concept::NumericVector3FloatingPoint T, template<class> class AGaussian3DDiagnoal>
+class BasicGaussian3DDiagnoalParameter final : public DistributionParameterBase<BasicGaussian3DDiagnoalParameter<T, AGaussian3DDiagnoal>,
+                                                                                AGaussian3DDiagnoal<T>> {
+private:
+    using VOfT = Utility::ValueTypeOf<T>;
+    using Base = DistributionParameterBase<BasicGaussian3DDiagnoalParameter<T, AGaussian3DDiagnoal>,
+                                           AGaussian3DDiagnoal<T>>;
+
+public:
+    constexpr BasicGaussian3DDiagnoalParameter();
+    constexpr BasicGaussian3DDiagnoalParameter(std::pair<VOfT, VOfT> pX, std::pair<VOfT, VOfT> pY, std::pair<VOfT, VOfT> pZ);
+
+    constexpr auto MuX() const { return fMuX; }
+    constexpr auto SigmaX() const { return fSigmaX; }
+    constexpr auto MuY() const { return fMuY; }
+    constexpr auto SigmaY() const { return fSigmaY; }
+    constexpr auto MuZ() const { return fMuZ; }
+    constexpr auto SigmaZ() const { return fSigmaZ; }
+
+    constexpr void MuX(VOfT muX) const { fMuX = muX; }
+    constexpr void SigmaX(VOfT sigmaX) const { fSigmaX = sigmaX; }
+    constexpr void MuY(VOfT muY) const { fMuY = muY; }
+    constexpr void SigmaY(VOfT sigmaY) const { fSigmaY = sigmaY; }
+    constexpr void MuZ(VOfT muZ) const { fMuZ = muZ; }
+    constexpr void SigmaZ(VOfT sigmaZ) const { fSigmaZ = sigmaZ; }
+
+    template<Concept::Character AChar>
+    friend auto operator<<(std::basic_ostream<AChar>& os, const BasicGaussian3DDiagnoalParameter& self) -> decltype(os);
+    template<Concept::Character AChar>
+    friend auto operator>>(std::basic_istream<AChar>& is, BasicGaussian3DDiagnoalParameter& self) -> decltype(is);
+
+private:
+    VOfT fMuX;
+    VOfT fSigmaX;
+    VOfT fMuY;
+    VOfT fSigmaY;
+    VOfT fMuZ;
+    VOfT fSigmaZ;
+};
+
+template<template<class> class ADerived, Concept::NumericVector3FloatingPoint T>
+class Gaussian3DDiagnoalBase : public RandomNumberDistributionBase<ADerived<T>,
+                                                                   BasicGaussian3DDiagnoalParameter<T, ADerived>,
+                                                                   T> {
+protected:
+    using VOfT = Utility::ValueTypeOf<T>;
+
+private:
+    using Base = RandomNumberDistributionBase<ADerived<T>,
+                                              BasicGaussian3DDiagnoalParameter<T, ADerived>,
+                                              T>;
+
+public:
+    constexpr Gaussian3DDiagnoalBase() = default;
+    constexpr Gaussian3DDiagnoalBase(std::pair<VOfT, VOfT> pX, std::pair<VOfT, VOfT> pY, std::pair<VOfT, VOfT> pZ);
+    constexpr explicit Gaussian3DDiagnoalBase(const typename Base::ParameterType& p);
+
+protected:
+    constexpr ~Gaussian3DDiagnoalBase() = default;
+
+public:
+    constexpr auto Parameter() const { return fParameter; }
+    constexpr auto MuX() const { return fParameter.MuX(); }
+    constexpr auto SigmaX() const { return fParameter.SigmaX(); }
+    constexpr auto MuY() const { return fParameter.MuY(); }
+    constexpr auto SigmaY() const { return fParameter.SigmaY(); }
+    constexpr auto MuZ() const { return fParameter.MuZ(); }
+    constexpr auto SigmaZ() const { return fParameter.SigmaZ(); }
+
+    constexpr void Parameter(const typename Base::ParameterType& p) { fParameter = p; }
+    constexpr void MuX(VOfT muX) const { fParameter.MuX(muX); }
+    constexpr void SigmaX(VOfT sigmaX) const { fParameter.SigmaX(sigmaX); }
+    constexpr void MuY(VOfT muY) const { fParameter.MuY(muY); }
+    constexpr void SigmaY(VOfT sigmaY) const { fParameter.SigmaY(sigmaY); }
+    constexpr void MuZ(VOfT muZ) const { fParameter.MuZ(muZ); }
+    constexpr void SigmaZ(VOfT sigmaZ) const { fParameter.SigmaZ(sigmaZ); }
+
+    template<Concept::Character AChar>
+    friend auto& operator<<(std::basic_ostream<AChar>& os, const Gaussian3DDiagnoalBase& self) { return os << self.fParameter; }
+    template<Concept::Character AChar>
+    friend auto& operator>>(std::basic_istream<AChar>& is, Gaussian3DDiagnoalBase& self) { return is >> self.fParameter; }
+
+protected:
+    typename Base::ParameterType fParameter;
+};
+
+} // namespace internal
+
+/// @brief
+/// @tparam T The result vector type. It must be 2-dimensional and has floating-point type.
+template<Concept::NumericVector3FloatingPoint T = std::array<double, 3>>
+class Gaussian3DDiagnoal;
+
+template<Concept::NumericVector3FloatingPoint T>
+using Gaussian3DDiagnoalParameter = internal::BasicGaussian3DDiagnoalParameter<T, Gaussian3DDiagnoal>;
+
+template<Concept::NumericVector3FloatingPoint T>
+class Gaussian3DDiagnoal final : public internal::Gaussian3DDiagnoalBase<Gaussian3DDiagnoal, T> {
+private:
+    using Base = internal::Gaussian3DDiagnoalBase<Gaussian3DDiagnoal, T>;
+    using VOfT = Base::VOfT;
+
+public:
+    using Base::Gaussian3DDiagnoalBase;
+
+    constexpr void Reset() { fSaved = false; }
+
+    auto operator()(UniformRandomBitGenerator auto& g) { return (*this)(g, this->fParameter); }
+    T operator()(UniformRandomBitGenerator auto& g, const Gaussian3DDiagnoalParameter<T>& p);
+
+    constexpr T Min() const { return {std::numeric_limits<VOfT>::lowest(), std::numeric_limits<VOfT>::lowest()}; }
+    constexpr T Max() const { return {std::numeric_limits<VOfT>::max(), std::numeric_limits<VOfT>::max()}; }
+
+    static constexpr auto Stateless() { return false; }
+
+private:
+    bool fSaved = false;
+    VOfT fSavedValue;
+};
+
+template<typename T, typename U, typename V>
+Gaussian3DDiagnoal(std::initializer_list<T>, std::initializer_list<U>, std::initializer_list<V>) -> Gaussian3DDiagnoal<std::array<std::common_type_t<T, U, V>, 3>>;
+
+/// @brief
+/// @tparam T The result vector type. It must be 2-dimensional and has floating-point type.
+template<Concept::NumericVector3FloatingPoint T = std::array<double, 3>>
+class Gaussian3DDiagnoalFast;
+
+template<Concept::NumericVector3FloatingPoint T>
+using Gaussian3DDiagnoalFastParameter = internal::BasicGaussian3DDiagnoalParameter<T, Gaussian3DDiagnoalFast>;
+
+template<Concept::NumericVector3FloatingPoint T>
+class Gaussian3DDiagnoalFast final : public internal::Gaussian3DDiagnoalBase<Gaussian3DDiagnoalFast, T> {
+private:
+    using Base = internal::Gaussian3DDiagnoalBase<Gaussian3DDiagnoalFast, T>;
+    using VOfT = Base::VOfT;
+
+public:
+    using Base::Gaussian3DDiagnoalBase;
+
+    constexpr void Reset() { fSaved = false; }
+
+    auto operator()(UniformRandomBitGenerator auto& g) { return (*this)(g, this->fParameter); }
+    T operator()(UniformRandomBitGenerator auto& g, const Gaussian3DDiagnoalFastParameter<T>& p);
+
+    constexpr T Min() const { return {std::numeric_limits<VOfT>::lowest(), std::numeric_limits<VOfT>::lowest()}; }
+    constexpr T Max() const { return {std::numeric_limits<VOfT>::max(), std::numeric_limits<VOfT>::max()}; }
+
+    static constexpr auto Stateless() { return false; }
+
+private:
+    bool fSaved = false;
+    VOfT fSavedValue;
+};
+
+template<typename T, typename U, typename V>
+Gaussian3DDiagnoalFast(std::initializer_list<T>, std::initializer_list<U>, std::initializer_list<V>) -> Gaussian3DDiagnoalFast<std::array<std::common_type_t<T, U, V>, 3>>;
+
+} // namespace MACE::Math::Random::Distribution
+
+#include "MACE/Math/Random/Distribution/Gaussian3DDiagnoal.inl"
---
+++ source/framework/MACE/Math/Random/Distribution/Gaussian3DDiagnoal.inl
@@ -0,0 +1,78 @@
+namespace MACE::Math::Random::Distribution {
+
+namespace internal {
+
+template<Concept::NumericVector3FloatingPoint T, template<class> class AGaussian3DDiagnoal>
+constexpr BasicGaussian3DDiagnoalParameter<T, AGaussian3DDiagnoal>::BasicGaussian3DDiagnoalParameter() :
+    BasicGaussian3DDiagnoalParameter({0, 1},
+                                     {0, 1},
+                                     {0, 1}) {}
+
+template<Concept::NumericVector3FloatingPoint T, template<class> class AGaussian3DDiagnoal>
+constexpr BasicGaussian3DDiagnoalParameter<T, AGaussian3DDiagnoal>::BasicGaussian3DDiagnoalParameter(std::pair<VOfT, VOfT> pX, std::pair<VOfT, VOfT> pY, std::pair<VOfT, VOfT> pZ) :
+    Base(),
+    fMuX(pX.first),
+    fSigmaX(pX.second),
+    fMuY(pY.first),
+    fSigmaY(pY.second),
+    fMuZ(pZ.first),
+    fSigmaZ(pZ.second) {}
+
+template<std::floating_point T, template<typename> class AGaussian2D, Concept::Character AChar>
+auto operator<<(std::basic_ostream<AChar>& os, BasicGaussian3DDiagnoalParameter<T, AGaussian2D> self) -> decltype(os) {
+    const auto oldPrecision = os.precision(std::numeric_limits<T>::max_digits10);
+    return os << self.fMuX << ' ' << self.fSigmaX << ' '
+              << self.fMuY << ' ' << self.fSigmaY << ' '
+              << self.fMuZ << ' ' << self.fSigmaZ
+              << std::setprecision(oldPrecision);
+}
+
+template<std::floating_point T, template<typename> class AGaussian2D, Concept::Character AChar>
+auto operator>>(std::basic_ostream<AChar>& is, BasicGaussian3DDiagnoalParameter<T, AGaussian2D> self) -> decltype(is) {
+    return is >>
+           self.fMuX >> self.fSigmaX >>
+           self.fMuY >> self.fSigmaY >>
+           self.fMuZ >> self.fSigmaZ;
+}
+
+template<template<class> class ADerived, Concept::NumericVector3FloatingPoint T>
+constexpr Gaussian3DDiagnoalBase<ADerived, T>::Gaussian3DDiagnoalBase(std::pair<VOfT, VOfT> pX, std::pair<VOfT, VOfT> pY, std::pair<VOfT, VOfT> pZ) :
+    Base(),
+    fParameter(pX, pY, pZ) {}
+
+template<template<class> class ADerived, Concept::NumericVector3FloatingPoint T>
+constexpr Gaussian3DDiagnoalBase<ADerived, T>::Gaussian3DDiagnoalBase(const typename Base::ParameterType& p) :
+    Base(),
+    fParameter(p) {}
+
+} // namespace internal
+
+#define MACE_MATH_RANDOM_DISTRIBUTION_GAUSSIAN_3D_DIAGNOAL_GENERATOR_SNIPPET(Suffix) \
+    static_assert(Gaussian2DDiagnoal##Suffix<std::array<VOfT, 2>>::Stateless());     \
+    Gaussian2DDiagnoal##Suffix<std::array<VOfT, 2>> standardGaussian2D;              \
+    const auto&& [u, v] = standardGaussian2D(g);                                     \
+    if ((fSaved = not fSaved)) {                                                     \
+        const auto&& [w, x] = standardGaussian2D(g);                                 \
+        fSavedValue = x;                                                             \
+        return {p.SigmaX() * u + p.MuX(),                                            \
+                p.SigmaY() * v + p.MuY(),                                            \
+                p.SigmaZ() * w + p.MuZ()};                                           \
+    } else {                                                                         \
+        return {p.SigmaX() * u + p.MuX(),                                            \
+                p.SigmaY() * v + p.MuY(),                                            \
+                p.SigmaZ() * fSavedValue + p.MuZ()};                                 \
+    }
+
+template<Concept::NumericVector3FloatingPoint T>
+T Gaussian3DDiagnoal<T>::operator()(UniformRandomBitGenerator auto& g, const Gaussian3DDiagnoalParameter<T>& p) {
+    MACE_MATH_RANDOM_DISTRIBUTION_GAUSSIAN_3D_DIAGNOAL_GENERATOR_SNIPPET()
+}
+
+template<Concept::NumericVector3FloatingPoint T>
+T Gaussian3DDiagnoalFast<T>::operator()(UniformRandomBitGenerator auto& g, const Gaussian3DDiagnoalFastParameter<T>& p) {
+    MACE_MATH_RANDOM_DISTRIBUTION_GAUSSIAN_3D_DIAGNOAL_GENERATOR_SNIPPET(Fast)
+}
+
+#undef MACE_MATH_RANDOM_DISTRIBUTION_GAUSSIAN_3D_DIAGNOAL_GENERATOR_SNIPPET
+
+} // namespace MACE::Math::Random::Distribution
