2afc2c5291f740a55f27e0ce0420282c776ad824
zhaoshihan <zhao_shihan@foxmail.com>
2023-01-17 20:09:04 +0800

    Add Math::Log

---
+++ source/framework/MACE/Math/Log.hxx
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "MACE/Math/RALog.hxx"
+
+#include <cmath>
+#include <concepts>
+
+namespace MACE::Math {
+
+template<std::floating_point T>
+constexpr T Log(T x) noexcept;
+template<std::floating_point T = double>
+constexpr auto Log(std::integral auto x) noexcept { return Log(static_cast<T>(x)); }
+
+template<std::floating_point T>
+constexpr T Log2(T x) noexcept;
+template<std::floating_point T = double>
+constexpr auto Log2(std::integral auto x) noexcept { return Log2(static_cast<T>(x)); }
+
+template<std::floating_point T>
+constexpr T Log10(T x) noexcept;
+template<std::floating_point T = double>
+constexpr auto Log10(std::integral auto x) noexcept { return Log10(static_cast<T>(x)); }
+
+} // namespace MACE::Math
+
+#include "MACE/Math/Log.inl"
