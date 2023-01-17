af1eecfa05c5d07a14e41f61f8a040fa41d67d88
zhaoshihan <zhao_shihan@foxmail.com>
2023-01-17 20:32:23 +0800

    Uniform distribution on disk

---
+++ source/framework/MACE/Math/Random/Distribution/UniformDisk.hxx
@@ -0,0 +1,158 @@
+#pragma once
+
+#include "MACE/Concept/InstantiatedFrom.hxx"
+#include "MACE/Concept/MathVector.hxx"
+#include "MACE/Concept/NumericVector.hxx"
+#include "MACE/Math/Hypot.hxx"
+#include "MACE/Math/IntegerPower.hxx"
+#include "MACE/Math/Random/Distribution/UniformRectangle.hxx"
+#include "MACE/Math/Random/RandomNumberDistributionBase.hxx"
+#include "MACE/Utility/ValueTypeOf.hxx"
+
+#include <array>
+#include <cmath>
+#include <concepts>
+#include <iomanip>
+
+namespace MACE::Math::Random::Distribution {
+
+namespace internal {
+
+template<Concept::NumericVector2Any T, template<class> class AUniformDisk>
+class BasicUniformDiskParameter final : public DistributionParameterBase<BasicUniformDiskParameter<T, AUniformDisk>,
+                                                                         AUniformDisk<T>> {
+private:
+    using VOfT = Utility::ValueTypeOf<T>;
+    using Base = DistributionParameterBase<BasicUniformDiskParameter<T, AUniformDisk>,
+                                           AUniformDisk<T>>;
+
+public:
+    constexpr BasicUniformDiskParameter();
+    constexpr BasicUniformDiskParameter(VOfT r, VOfT x0, VOfT y0);
+    constexpr BasicUniformDiskParameter(VOfT radius, const T& center);
+    constexpr explicit BasicUniformDiskParameter(VOfT radius);
+
+    constexpr const auto& Radius() const { return fRadius; }
+    constexpr const auto& CenterX() const { return fCenterX; }
+    constexpr const auto& CenterY() const { return fCenterY; }
+    constexpr T Center() const { return {fCenterX, fCenterY}; }
+
+    constexpr void Radius(VOfT r) { fRadius = r; }
+    constexpr void CenterX(VOfT x0) const { fCenterX = x0; }
+    constexpr void CenterY(VOfT y0) const { fCenterY = y0; }
+    constexpr void Center(VOfT x0, VOfT y0) { (CenterX(x0), CenterY(y0)); }
+    constexpr void Center(const T& r0) { Center(r0[0], r0[1]); }
+
+    template<Concept::Character AChar>
+    friend auto operator<<(std::basic_ostream<AChar>& os, const BasicUniformDiskParameter<T, AUniformDisk>& self) -> decltype(os);
+    template<Concept::Character AChar>
+    friend auto operator>>(std::basic_istream<AChar>& is, BasicUniformDiskParameter<T, AUniformDisk>& self) -> decltype(is);
+
+private:
+    VOfT fRadius;
+    VOfT fCenterX;
+    VOfT fCenterY;
+};
+
+template<Concept::NumericVector2Any T, template<typename> class AUniformDisk>
+class UniformDiskBase : public RandomNumberDistributionBase<AUniformDisk<T>,
+                                                            BasicUniformDiskParameter<T, AUniformDisk>,
+                                                            T> {
+private:
+    using VOfT = Utility::ValueTypeOf<T>;
+    using Base = RandomNumberDistributionBase<AUniformDisk<T>,
+                                              BasicUniformDiskParameter<T, AUniformDisk>,
+                                              T>;
+
+public:
+    constexpr UniformDiskBase() = default;
+    constexpr UniformDiskBase(VOfT r, VOfT x0, VOfT y0);
+    constexpr UniformDiskBase(VOfT radius, const T& center);
+    constexpr explicit UniformDiskBase(VOfT radius);
+    constexpr explicit UniformDiskBase(const typename Base::ParameterType& p);
+
+protected:
+    constexpr ~UniformDiskBase() = default;
+
+public:
+    constexpr void Reset() {}
+
+    constexpr auto Parameter() const { return fParameter; }
+    constexpr const auto& Radius() const { return fParameter.Radius(); }
+    constexpr const auto& CenterX() const { return fParameter.CenterX(); }
+    constexpr const auto& CenterY() const { return fParameter.CenterY(); }
+    constexpr auto Center() const { return fParameter.Center(); }
+
+    constexpr void Parameter(const typename Base::ParameterType& p) { fParameter = p; }
+    constexpr void Radius(VOfT r) { fParameter.Radius(r); }
+    constexpr void CenterX(VOfT x0) const { fParameter.CenterX(x0); }
+    constexpr void CenterY(VOfT y0) const { fParameter.CenterY(y0); }
+    constexpr void Center(VOfT x0, VOfT y0) { fParameter.Center(x0, y0); }
+    constexpr void Center(const T& r0) { fParameter.Center(r0); }
+
+    constexpr auto Min() const { return T{CenterX() - Radius(), CenterY() - Radius()}; }
+    constexpr auto Max() const { return T{CenterX() + Radius(), CenterY() + Radius()}; }
+
+    static constexpr auto Stateless() { return true; }
+
+    template<Concept::Character AChar>
+    friend auto& operator<<(std::basic_ostream<AChar>& os, const UniformDiskBase<T, AUniformDisk>& self) { return os << self.fParameter; }
+    template<Concept::Character AChar>
+    friend auto& operator>>(std::basic_istream<AChar>& is, UniformDiskBase<T, AUniformDisk>& self) { return is >> self.fParameter; }
+
+protected:
+    static constexpr std::pair<T, VOfT> UniformCompactDisk(UniformRandomBitGenerator auto& g, const DistributionParameter auto& p);
+
+protected:
+    typename Base::ParameterType fParameter;
+};
+
+} // namespace internal
+
+template<Concept::NumericVector2Any T = std::array<double, 2>>
+class UniformCompactDisk;
+
+template<Concept::NumericVector2Any T>
+using UniformCompactDiskParameter = internal::BasicUniformDiskParameter<T, UniformCompactDisk>;
+
+template<Concept::NumericVector2Any T>
+class UniformCompactDisk final : public internal::UniformDiskBase<T, UniformCompactDisk> {
+public:
+    using internal::UniformDiskBase<T, UniformCompactDisk>::UniformDiskBase;
+
+    constexpr auto operator()(UniformRandomBitGenerator auto& g) { return (*this)(g, this->fParameter); }
+    constexpr T operator()(UniformRandomBitGenerator auto& g, const UniformCompactDiskParameter<T>& p);
+};
+
+template<typename VOfT, class T>
+UniformCompactDisk(VOfT, T) -> UniformCompactDisk<std::enable_if_t<std::same_as<VOfT, Utility::ValueTypeOf<std::remove_cvref_t<T>>>, std::remove_cvref_t<T>>>;
+template<typename T, typename U, typename V>
+UniformCompactDisk(T, U, V) -> UniformCompactDisk<std::array<std::common_type_t<T, U, V>, 2>>;
+template<typename T>
+UniformCompactDisk(T) -> UniformCompactDisk<std::array<T, 2>>;
+
+template<Concept::NumericVector2Any T = std::array<double, 2>>
+class UniformDisk;
+
+template<Concept::NumericVector2Any T>
+using UniformDiskParameter = internal::BasicUniformDiskParameter<T, UniformDisk>;
+
+template<Concept::NumericVector2Any T>
+class UniformDisk final : public internal::UniformDiskBase<T, UniformDisk> {
+public:
+    using internal::UniformDiskBase<T, UniformDisk>::UniformDiskBase;
+
+    constexpr auto operator()(UniformRandomBitGenerator auto& g) { return (*this)(g, this->fParameter); }
+    constexpr T operator()(UniformRandomBitGenerator auto& g, const UniformDiskParameter<T>& p);
+};
+
+template<typename VOfT, class T>
+UniformDisk(VOfT, T) -> UniformDisk<std::enable_if_t<std::same_as<VOfT, Utility::ValueTypeOf<std::remove_cvref_t<T>>>, std::remove_cvref_t<T>>>;
+template<typename T, typename U, typename V>
+UniformDisk(T, U, V) -> UniformDisk<std::array<std::common_type_t<T, U, V>, 2>>;
+template<typename T>
+UniformDisk(T) -> UniformDisk<std::array<T, 2>>;
+
+} // namespace MACE::Math::Random::Distribution
+
+#include "MACE/Math/Random/Distribution/UniformDisk.inl"
---
+++ source/framework/MACE/Math/Random/Distribution/UniformDisk.inl
@@ -0,0 +1,87 @@
+namespace MACE::Math::Random::Distribution {
+
+namespace internal {
+
+template<Concept::NumericVector2Any T, template<class> class AUniformDisk>
+constexpr BasicUniformDiskParameter<T, AUniformDisk>::BasicUniformDiskParameter() :
+    BasicUniformDiskParameter(1) {}
+
+template<Concept::NumericVector2Any T, template<class> class AUniformDisk>
+constexpr BasicUniformDiskParameter<T, AUniformDisk>::BasicUniformDiskParameter(VOfT r, VOfT x0, VOfT y0) :
+    Base(),
+    fRadius(r),
+    fCenterX(x0),
+    fCenterY(y0) {}
+
+template<Concept::NumericVector2Any T, template<class> class AUniformDisk>
+constexpr BasicUniformDiskParameter<T, AUniformDisk>::BasicUniformDiskParameter(VOfT radius, const T& center) :
+    Base(),
+    fRadius(radius),
+    fCenterX(center[0]),
+    fCenterY(center[1]) {}
+
+template<Concept::NumericVector2Any T, template<class> class AUniformDisk>
+constexpr BasicUniformDiskParameter<T, AUniformDisk>::BasicUniformDiskParameter(VOfT radius) :
+    Base(),
+    fRadius(radius),
+    fCenterX(0),
+    fCenterY(0) {}
+
+template<std::floating_point T, template<typename> class AUniformDisk, Concept::Character AChar>
+auto operator<<(std::basic_ostream<AChar>& os, const BasicUniformDiskParameter<T, AUniformDisk>& self) -> decltype(os) {
+    const auto oldPrecision = os.precision(std::numeric_limits<T>::max_digits10);
+    return os << self.fRadius << ' ' << self.fCenter[0] << ' ' << self.fCenter[1] << std::setprecision(oldPrecision);
+}
+
+template<Concept::NumericVector2Any T, template<class> class AUniformDisk>
+constexpr UniformDiskBase<T, AUniformDisk>::UniformDiskBase(VOfT r, VOfT x0, VOfT y0) :
+    Base(),
+    fParameter(r, x0, y0) {}
+
+template<Concept::NumericVector2Any T, template<class> class AUniformDisk>
+constexpr UniformDiskBase<T, AUniformDisk>::UniformDiskBase(VOfT radius, const T& center) :
+    Base(),
+    fParameter(radius, center) {}
+
+template<Concept::NumericVector2Any T, template<class> class AUniformDisk>
+constexpr UniformDiskBase<T, AUniformDisk>::UniformDiskBase(VOfT radius) :
+    Base(),
+    fParameter(radius) {}
+
+template<Concept::NumericVector2Any T, template<class> class AUniformDisk>
+constexpr UniformDiskBase<T, AUniformDisk>::UniformDiskBase(const typename Base::ParameterType& p) :
+    Base(),
+    fParameter(p) {}
+
+} // namespace internal
+
+#define MACE_MATH_RANDOM_DISTRIBUTION_UNIFORM_DISK_GENERATOR(rejection) \
+    T r;                                                                \
+    Utility::ValueTypeOf<T> r2;                                         \
+    do {                                                                \
+        r = UniformCompactRectangle<T>({-0.5, 0.5}, {-0.5, 0.5})(g);    \
+        r2 = Math::Hypot2(r[0], r[1]);                                  \
+        std2b::assume(0 <= r2 and r2 <= 0.5);                           \
+    } while (rejection);                                                \
+    if constexpr (Concept::MathVector2Any<T>) {                         \
+        r *= 2 * p.Radius();                                            \
+        r += p.Center();                                                \
+    } else {                                                            \
+        r[0] = 2 * p.Radius() * r[0] + p.Center()[0];                   \
+        r[1] = 2 * p.Radius() * r[1] + p.Center()[1];                   \
+    }                                                                   \
+    return r;
+
+template<Concept::NumericVector2Any T>
+constexpr T UniformCompactDisk<T>::operator()(UniformRandomBitGenerator auto& g, const UniformCompactDiskParameter<T>& p) {
+    MACE_MATH_RANDOM_DISTRIBUTION_UNIFORM_DISK_GENERATOR(r2 > 0.25)
+}
+
+template<Concept::NumericVector2Any T>
+constexpr T UniformDisk<T>::operator()(UniformRandomBitGenerator auto& g, const UniformDiskParameter<T>& p) {
+    MACE_MATH_RANDOM_DISTRIBUTION_UNIFORM_DISK_GENERATOR(r2 >= 0.25)
+}
+
+#undef MACE_MATH_RANDOM_DISTRIBUTION_UNIFORM_DISK_GENERATOR
+
+} // namespace MACE::Math::Random::Distribution
