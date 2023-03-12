c736d88ca47282281c390936a2128199f684685e
zhaoshihan <zhao_shihan@foxmail.com>
2023-03-12 12:05:21 +0800

    Update ValueTypeOf

--- source/framework/MACE/Utility/ValueTypeOf.hxx
+++ source/framework/MACE/Utility/ValueTypeOf.hxx
@@ -14,30 +14,30 @@ namespace MACE::Utility {
 namespace internal {
 
 template<class T>
-struct ValueTypeOf {};
+struct ValueTypeOfImpl {};
 
 template<class T>
     requires requires { typename T::value_type; }
-struct ValueTypeOf<T> {
+struct ValueTypeOfImpl<T> {
     using Type = typename T::value_type;
 };
 
 template<std::ranges::input_range T>
     requires(not requires { typename T::value_type; })
-struct ValueTypeOf<T> {
-    using Type = std::remove_reference_t<decltype(*std::ranges::cbegin(std::declval<T&>()))>;
+struct ValueTypeOfImpl<T> {
+    using Type = std::remove_reference_t<decltype(*std::declval<std::ranges::iterator_t<T>>())>;
 };
 
 template<Concept::InputNumericVectorAny T>
     requires(not requires { typename T::value_type; } and
              not std::ranges::input_range<T>)
-struct ValueTypeOf<T> {
+struct ValueTypeOfImpl<T> {
     using Type = std::remove_reference_t<decltype(std::declval<T&>()[std::declval<gsl::index>()])>;
 };
 
 } // namespace internal
 
 template<class T>
-using ValueTypeOf = typename internal::ValueTypeOf<T>::Type;
+using ValueTypeOf = typename internal::ValueTypeOfImpl<T>::Type;
 
 } // namespace MACE::Utility
