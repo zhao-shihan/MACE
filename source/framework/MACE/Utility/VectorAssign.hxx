16667d027bf61a21e34a644d6c6d4267bf3eced2
zhaoshihan <zhao_shihan@foxmail.com>
2023-03-12 12:05:46 +0800

    Update VectorAssign and VectorCast

---
+++ source/framework/MACE/Utility/VectorAssign.hxx
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "MACE/Concept/FundamentalType.hxx"
+#include "MACE/Concept/Indirectable.hxx"
+#include "MACE/Concept/InputNumericVector.hxx"
+#include "MACE/Concept/NumericVector.hxx"
+#include "MACE/Utility/ToSigned.hxx"
+#include "MACE/Utility/ValueTypeOf.hxx"
+
+#include "gsl/gsl"
+
+#include <concepts>
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+namespace MACE::Utility {
+
+/// @brief Assign something to a vector. If lhs = rhs is well-formed, assign
+/// with operator=, else assign element by element.
+/// e.g. VectorAssign<double, 3>(u, v); VectorAssign<double, 3>(u, {x, y, z});
+/// @param lhs The vector at left-hand side.
+/// @param rhs Something at right hand side.
+/// @return If lhs = rhs is well-formed, returns lhs = rhs, else returns the
+/// lvalue reference to lhs.
+decltype(auto) VectorAssign(Concept::NumericVectorAny auto& lhs, auto&& rhs)
+    requires(std::assignable_from<decltype(lhs), decltype(rhs)>)
+{
+    return lhs = std::forward<decltype(rhs)>(rhs);
+}
+
+/// @brief Assign something to a vector. If lhs = rhs is well-formed, assign
+/// with operator=, else assign element by element.
+/// e.g. VectorAssign<double, 3>(u, v); VectorAssign<double, 3>(u, {x, y, z});
+/// @param lhs The vector at left-hand side.
+/// @param rhs Something at right hand side.
+/// @return If lhs = rhs is well-formed, returns lhs = rhs, else returns the
+/// lvalue reference to lhs.
+decltype(auto) VectorAssign(Concept::NumericVectorAny auto& lhs, std::ranges::input_range auto&& rhs)
+    requires(not std::assignable_from<decltype(lhs), decltype(rhs)> and
+             std::assignable_from<ValueTypeOf<decltype(lhs)>&, ValueTypeOf<decltype(rhs)>>)
+{
+    for (gsl::index i = 0;
+         auto&& value : std::forward<decltype(rhs)>(rhs)) {
+        lhs[i] = std::forward<decltype(value)>(value);
+        ++i;
+    }
+    return lhs;
+}
+
+/// @brief Assign something to a vector. If lhs = rhs is well-formed, assign
+/// with operator=, else assign element by element.
+/// e.g. VectorAssign<double, 3>(u, v); VectorAssign<double, 3>(u, {x, y, z});
+/// @param lhs The vector at left-hand side.
+/// @param rhs Something at right hand side.
+/// @return If lhs = rhs is well-formed, returns lhs = rhs, else returns the
+/// lvalue reference to lhs.
+decltype(auto) VectorAssign(Concept::NumericVectorAny auto& lhs, Concept::InputNumericVectorAny auto&& rhs)
+    requires(not std::assignable_from<decltype(lhs), decltype(rhs)> and
+             not std::ranges::input_range<decltype(rhs)>)
+{
+    constexpr auto dimension = sizeof(decltype(lhs)) / sizeof(ValueTypeOf<decltype(lhs)>);
+    for (gsl::index i = 0; i < ToSigned(dimension); ++i) {
+        lhs[i] = std::forward<decltype(rhs)>(rhs)[i];
+    }
+    return lhs;
+}
+
+} // namespace MACE::Utility
--- source/framework/MACE/Utility/VectorCast.hxx
+++ source/framework/MACE/Utility/VectorCast.hxx
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "MACE/Concept/NumericVector.hxx"
-#include "MACE/Utility/AssignVector.hxx"
 #include "MACE/Utility/ValueTypeOf.hxx"
+#include "MACE/Utility/VectorAssign.hxx"
 
 #include <concepts>
 #include <type_traits>
@@ -10,59 +10,18 @@
 
 namespace MACE::Utility {
 
-template<class T, std::size_t N>
-    requires Concept::NumericVectorAny<T, N>
-[[nodiscard]] T VectorCast(T&& src) {
-    return src;
-}
-
-template<class T, std::size_t N>
-    requires Concept::NumericVectorAny<T, N>
+template<Concept::NumericVectorAny T>
 [[nodiscard]] T VectorCast(std::convertible_to<T> auto&& src) {
-    return src;
+    return std::forward<decltype(src)>(src);
 }
 
-template<class T, std::size_t N>
-    requires Concept::NumericVectorAny<T, N>
+template<Concept::NumericVectorAny T>
 [[nodiscard]] T VectorCast(auto&& src)
     requires(not std::convertible_to<decltype(src), T>)
 {
     T dst;
-    AssignVector<ValueTypeOf<T>, N>(dst, std::forward<decltype(src)>(src));
+    VectorAssign(dst, std::forward<decltype(src)>(src));
     return dst;
-}
-
-template<class T, std::size_t N>
-    requires Concept::NumericVectorAny<T, N>
-[[nodiscard]] T VectorCast(auto&&... args)
-    requires(std::same_as<std::common_type_t<T, decltype(args)...>, T> and
-             sizeof...(args) == N)
-{
-    return {std::forward<decltype(args)>(args)...};
-}
-
-template<Concept::NumericVectorAny<2> T>
-[[nodiscard]] T Vector2Cast(auto&&... args)
-    requires(sizeof...(args) == 1 or
-             sizeof...(args) == 2)
-{
-    return VectorCast<T, 2>(std::forward<decltype(args)>(args)...);
-}
-
-template<Concept::NumericVectorAny<3> T>
-[[nodiscard]] T Vector3Cast(auto&&... args)
-    requires(sizeof...(args) == 1 or
-             sizeof...(args) == 3)
-{
-    return VectorCast<T, 3>(std::forward<decltype(args)>(args)...);
-}
-
-template<Concept::NumericVectorAny<4> T>
-[[nodiscard]] T Vector4Cast(auto&&... args)
-    requires(sizeof...(args) == 1 or
-             sizeof...(args) == 4)
-{
-    return VectorCast<T, 4>(std::forward<decltype(args)>(args)...);
 }
 
 } // namespace MACE::Utility
