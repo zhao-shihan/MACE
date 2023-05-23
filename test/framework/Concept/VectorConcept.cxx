2d579b5699c366121bf40e66aed9a356e0a4a8da
zhaoshihan <zhao_shihan@foxmail.com>
2023-05-23 22:49:31 +0800

    Update vector concept test

--- test/framework/Concept/VectorConcept.cxx
+++ test/framework/Concept/VectorConcept.cxx
@@ -1,6 +1,6 @@
 #include "MACE/Concept/MathVector.hxx"
 #include "MACE/Concept/NumericVector.hxx"
-#include "MACE/Extension/stdx/array_alias.hxx"
+#include "MACE/Extension/stdx/arraynx.hxx"
 
 #include "CLHEP/Vector/LorentzVector.h"
 #include "CLHEP/Vector/ThreeVector.h"
@@ -18,30 +18,30 @@ namespace stdx = MACE::stdx;
 using namespace MACE::Compatibility::CLHEPDefectFix::Hep2VectorDivisionAssignment;
 using namespace MACE::Concept;
 
-static_assert(InputNumericVector<float*, float, 3>);
-static_assert(InputNumericVector<double*, double, 3>);
-static_assert(InputNumericVector<long double*, long double, 3>);
+static_assert(InputVector<float*, float, 3>);
+static_assert(InputVector<double*, double, 3>);
+static_assert(InputVector<long double*, long double, 3>);
 static_assert(not NumericVector<float*, float, 3>);
 static_assert(not NumericVector<double*, double, 3>);
 static_assert(not NumericVector<long double*, long double, 3>);
 
-static_assert(InputNumericVector<const float*, float, 3>);
-static_assert(InputNumericVector<const double*, double, 3>);
-static_assert(InputNumericVector<const long double*, long double, 3>);
+static_assert(InputVector<const float*, float, 3>);
+static_assert(InputVector<const double*, double, 3>);
+static_assert(InputVector<const long double*, long double, 3>);
 static_assert(not NumericVector<const float*, float, 3>);
 static_assert(not NumericVector<const double*, double, 3>);
 static_assert(not NumericVector<const long double*, long double, 3>);
 
-static_assert(InputNumericVector<float[3], float, 3>);
-static_assert(InputNumericVector<double[3], double, 3>);
-static_assert(InputNumericVector<long double[3], long double, 3>);
+static_assert(InputVector<float[3], float, 3>);
+static_assert(InputVector<double[3], double, 3>);
+static_assert(InputVector<long double[3], long double, 3>);
 static_assert(not NumericVector<float[3], float, 3>);
 static_assert(not NumericVector<double[3], double, 3>);
 static_assert(not NumericVector<long double[3], long double, 3>);
 
-static_assert(InputNumericVector<const float[3], float, 3>);
-static_assert(InputNumericVector<const double[3], double, 3>);
-static_assert(InputNumericVector<const long double[3], long double, 3>);
+static_assert(InputVector<const float[3], float, 3>);
+static_assert(InputVector<const double[3], double, 3>);
+static_assert(InputVector<const long double[3], long double, 3>);
 static_assert(not NumericVector<const float[3], float, 3>);
 static_assert(not NumericVector<const double[3], double, 3>);
 static_assert(not NumericVector<const long double[3], long double, 3>);
