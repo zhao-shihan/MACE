878e55cb06a3bde7d5cc3e361a7941a2bf90f320
zhaoshihan <zhao_shihan@foxmail.com>
2023-05-29 16:21:27 +0800

    Update stdx::tuple_like

--- source/framework/MACE/Extension/stdx/tuple_like.hxx
+++ source/framework/MACE/Extension/stdx/tuple_like.hxx
@@ -8,8 +8,8 @@ namespace MACE::inline Extension::stdx {
 
 namespace internal {
 
-template<class T, std::size_t N>
-concept has_tuple_element =
+template<typename T, std::size_t N>
+concept has_tuple_element_and_get =
     requires(T t) {
         typename std::tuple_element_t<N, std::remove_const_t<T>>;
         { std::get<N>(t) } -> std::convertible_to<const std::tuple_element_t<N, T>&>;
@@ -17,18 +17,17 @@ concept has_tuple_element =
 
 } // namespace internal
 
-template<class T>
+template<typename T>
 concept tuple_like =
-    not std::is_reference_v<T> and
-    requires(T t) {
+    requires {
+        requires not std::is_reference_v<T>;
         typename std::tuple_size<T>::type;
         requires std::derived_from<std::tuple_size<T>,
                                    std::integral_constant<std::size_t, std::tuple_size_v<T>>>;
-    } and []<std::size_t... N>(std::index_sequence<N...>) {
-        return (... and internal::has_tuple_element<T, N>);
-    }(std::make_index_sequence<std::tuple_size_v<T>>());
-
-template<class T>
-concept pair_like = tuple_like<T> and std::tuple_size_v<T> == 2;
+        requires
+            []<std::size_t... N>(std::index_sequence<N...>) {
+                return (... and internal::has_tuple_element_and_get<T, N>);
+            }(std::make_index_sequence<std::tuple_size_v<T>>());
+    };
 
 } // namespace MACE::inline Extension::stdx
