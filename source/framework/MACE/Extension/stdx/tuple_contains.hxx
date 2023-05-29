9782de0e1ef1fc48893c6efb6bd0823632ef71d9
zhaoshihan <zhao_shihan@foxmail.com>
2023-05-29 16:23:15 +0800

    Add stdx::tuple_contains and tuple_contains_unique

---
+++ source/framework/MACE/Extension/stdx/tuple_contains.hxx
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "MACE/Extension/stdx/tuple_count.hxx"
+#include "MACE/Extension/stdx/tuple_like.hxx"
+
+#include <type_traits>
+
+namespace MACE::inline Extension::stdx {
+
+template<tuple_like T, typename U>
+struct tuple_contains
+    : std::conditional_t<tuple_count_v<T, U> >= 1,
+                         std::true_type,
+                         std::false_type> {};
+
+template<tuple_like T, typename U>
+inline constexpr auto tuple_contains_v = tuple_contains<T, U>::value;
+
+template<tuple_like T, typename U>
+struct tuple_contains_unique
+    : std::conditional_t<tuple_count_v<T, U> == 1,
+                         std::true_type,
+                         std::false_type> {};
+
+template<tuple_like T, typename U>
+inline constexpr auto tuple_contains_unique_v = tuple_contains_unique<T, U>::value;
+
+} // namespace MACE::inline Extension::stdx
