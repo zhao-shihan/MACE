b8d7911cbcfd70297acbeb3165bc9fd5de74e0c0
zhaoshihan <zhao_shihan@foxmail.com>
2023-05-31 20:31:26 +0800

    Edit InstantiatedFrom

--- source/framework/MACE/Concept/InstantiatedFrom.hxx
+++ source/framework/MACE/Concept/InstantiatedFrom.hxx
@@ -5,26 +5,28 @@
 namespace MACE::Concept {
 
 // Work around LLVM issue #57142 (https://github.com/llvm/llvm-project/issues/57142)
-#if not defined __clang__ or __clang_major__ <= 12
+// #if not defined __clang__ or __clang_major__ <= 12
 
 namespace internal {
 
-template<template<typename...> class, typename>
-struct IsTemplateOf : std::false_type {};
+template<template<typename...> typename, typename>
+struct IsTemplateOf
+    : std::false_type {};
 
-template<template<typename...> class ATemplate, typename... Args>
-struct IsTemplateOf<ATemplate, ATemplate<Args...>> : std::true_type {};
+template<template<typename...> typename ATemplate, typename... Args>
+struct IsTemplateOf<ATemplate, ATemplate<Args...>>
+    : std::true_type {};
 
 } // namespace internal
 
-template<class T, template<typename...> class ATemplate>
+template<typename T, template<typename...> typename ATemplate>
 concept InstantiatedFrom = internal::IsTemplateOf<ATemplate, T>::value;
 
-#else
+// #else
 
-template<class T, template<typename...> class>
-concept InstantiatedFrom = std::is_class_v<T>;
+// template<typename T, template<typename...> typename>
+// concept InstantiatedFrom = std::is_class_v<T>;
 
-#endif
+// #endif
 
 } // namespace MACE::Concept
