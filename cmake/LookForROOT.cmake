6c6416bfa62de314c2fb46c33fcbf471925e8371
zhaoshihan <zhao_shihan@foxmail.com>
2023-10-30 02:07:14 +0800

    Fix GenFit build

--- cmake/LookForROOT.cmake
+++ cmake/LookForROOT.cmake
@@ -1,5 +1,9 @@
 message(STATUS "Looking for ROOT")
 
-find_package(ROOT 6.28.04 REQUIRED Geom)
+if(MACE_BUILTIN_GENFIT_EVENT_DISPLAY)
+    find_package(ROOT 6.28.04 REQUIRED Geom Eve)
+else()
+    find_package(ROOT 6.28.04 REQUIRED Geom)
+endif()
 
 message(STATUS "Looking for ROOT - found (version: ${ROOT_VERSION})")
