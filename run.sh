c1597e7b1fc91669a63a318dea4efbd9ef57d3ae
chen.siyuan <ischen.siyuan@foxmail.com>
2024-06-03 22:06:02 +0800

    Update energy resolution test version

--- run.sh
+++ run.sh
@@ -3,42 +3,42 @@
 V=pen-5cm
 echo "$V is running..."
 cd ~/work/mace/$V/src/app/simulation/MACE/PhaseI/SimMACEPhaseI
-mpirun -n 16 ./SimMACEPhaseI test.mac
+mpirun ./SimMACEPhaseI test.mac
 hadd -ff $V.root typePEN/*.root
 mv $V.root ~/work/mace/lengthTest
 
 V=pen-6cm
 echo "$V is running..."
 cd ~/work/mace/$V/src/app/simulation/MACE/PhaseI/SimMACEPhaseI
-mpirun -n 16 ./SimMACEPhaseI test.mac
+mpirun ./SimMACEPhaseI test.mac
 hadd -ff $V.root typePEN/*.root
 mv $V.root ~/work/mace/lengthTest
 
 V=pen-7cm
 echo "$V is running..."
 cd ~/work/mace/$V/src/app/simulation/MACE/PhaseI/SimMACEPhaseI
-mpirun -n 16 ./SimMACEPhaseI test.mac
+mpirun ./SimMACEPhaseI test.mac
 hadd -ff $V.root typePEN/*.root
 mv $V.root ~/work/mace/lengthTest
 
 V=pen-8cm
 echo "$V is running..."
 cd ~/work/mace/$V/src/app/simulation/MACE/PhaseI/SimMACEPhaseI
-mpirun -n 16 ./SimMACEPhaseI test.mac
+mpirun ./SimMACEPhaseI test.mac
 hadd -ff $V.root typePEN/*.root
 mv $V.root ~/work/mace/lengthTest
 
 V=pen-9cm
 echo "$V is running..."
 cd ~/work/mace/$V/src/app/simulation/MACE/PhaseI/SimMACEPhaseI
-mpirun -n 16 ./SimMACEPhaseI test.mac
+mpirun ./SimMACEPhaseI test.mac
 hadd -ff $V.root typePEN/*.root
 mv $V.root ~/work/mace/lengthTest
 
 V=pen-10cm
 echo "$V is running..."
 cd ~/work/mace/$V/src/app/simulation/MACE/PhaseI/SimMACEPhaseI
-mpirun -n 16 ./SimMACEPhaseI test.mac
+mpirun ./SimMACEPhaseI test.mac
 hadd -ff $V.root typePEN/*.root
 mv $V.root ~/work/mace/lengthTest
 
