8e2ac62c0f84ae7f4bb07a90679d248517b879c2
wzc <2292250560@qq.com>
2025-07-28 21:15:12 +0800

    Write primary test script.

--- module_test.sh
+++ module_test.sh
@@ -1,8 +1,18 @@
 # build
 mkdir build && cd build && cmake .. && make
 
+source data/mace_offline_data.sh 
+
 # modules
-modules=("GenMuonICDecay" "GenMuoniumICDecay" "MakeGeometry" "PhaseI" "ReconECAL" "ReconMMSTrack" "SimDose" "SimECAL" "SimMACE" "SimMMS" "SimPTS" "SimTTC" "SimTarget" "SimVeto" "SmearMACE")
+./MACE GenMuonICDecay -d 1 -s 2 -o "test_mu2eeevv_$(date "+%Y%m%d-%H:%M").root" 1000
+./MACE GenMuoniumICDecay -d 1 -s 2 -o "test_M2eeevv_$(date "+%Y%m%d-%H:%M").root" 1000
+./MACE PhaseI SimMACEPhaseI 
+./MACE SmearMACE 1000
+# subcommand ReconECAL, ReconSciFi not tested. ReconECAL need SimMACEPhaseI_geom.yaml.  
+# ReconMMSTrack: empty list of input files.
+# ReconECAL: empty list of input files.
+
+modules=("SimDose" "SimECAL" "SimMACE" "SimMMS" "SimPTS" "SimTTC" "SimTarget" "SimVeto")
 for module in "${modules[@]}"; do
     echo "+++> testing: $module <+++"
     ./MACE SimMACE $module
