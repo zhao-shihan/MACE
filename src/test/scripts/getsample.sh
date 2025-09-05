6feb70be5793a7206d8f7f00a832fa86fdd36edb
wzc <2292250560@qq.com>
2025-09-05 09:58:44 +0800

    get sample script -q option to exit root env.

--- src/test/scripts/getsample.sh
+++ src/test/scripts/getsample.sh
@@ -4,19 +4,23 @@ mkdir "$SAMPLING_DIR" && cd $_
 source ../../data/mace_offline_data.sh 
 
 ../../MACE SimMMS ../../SimMMS/run_em_flat.mac 
-root '../GetCDCSimHit.cxx("SimMMS_em_flat","SimMMS_em_flat.root","Sample.root")'
+root -l -q '../GetCDCSimHit.cxx("SimMMS_em_flat","SimMMS_em_flat.root","Sample.root")'
+root -l -q '../GetMMSSimTrack.cxx("SimMMS_em_flat","SimMMS_em_flat.root","Sample.root")'
 
 ../../MACE SimVeto ../../SimVeto/run_hit_partial.mac
-root '../GetVetoSimHit.cxx("SimVeto_hit_partial","SimVeto_hit_partial.root","Sample.root")'
+root -l -q '../GetVetoSimHit.cxx("SimVeto_hit_partial","SimVeto_hit_partial.root","Sample.root")'
 
 ../../MACE SimTTC ../../SimTTC/run_em_flat.mac
-root '../GetTTCSimHit.cxx("SimTTC_em_flat","SimTTC_em_flat.root","Sample.root")'
-
-../../MACE SimECAL ../../SimECAL/test.mac 
-root '../GetECALSimHit.cxx("SimECAL_test","SimECAL_test.root","Sample.root")'
+root -l -q '../GetTTCSimHit.cxx("SimTTC_em_flat","SimTTC_em_flat.root","Sample.root")'
 
 ../../MACE SimMACE ../../SimMACE/run_signal.mac
-root '../GetMCPSimHit.cxx("SimMACE_signal","SimMACE_signal.root","Sample.root")'
+root -l -q '../GetMCPSimHit.cxx("SimMACE_signal","SimMACE_signal.root","Sample.root")'
+root -l -q '../GetTTCSimHit.cxx("SimMACE_signal","SimMACE_signal.root","Sample.root")'
+root -l -q '../GetCDCSimHit.cxx("SimMACE_signal","SimMACE_signal.root","Sample.root")'
+root -l -q '../GetMMSSimTrack.cxx("SimMACE_signal","SimMACE_signal.root","Sample.root")'
 
-../../MACE PhaseI SimMACEPhaseI ../../PhaseI/SimMACEPhaseI/run_M2ee.mac
-root '../GetMRPCSimHit.cxx("SimMACEPhaseI_M2ee","SimMACEPhaseI_M2ee.root","Sample.root")'
+# ../../MACE SimECAL ../../SimECAL/test.mac 
+# root '../GetECALSimHit.cxx("SimECAL_test","SimECAL_test.root","Sample.root")'
+
+# ../../MACE PhaseI SimMACEPhaseI ../../PhaseI/SimMACEPhaseI/run_M2ee.mac
+# root '../GetMRPCSimHit.cxx("SimMACEPhaseI_M2ee","SimMACEPhaseI_M2ee.root","Sample.root")'
