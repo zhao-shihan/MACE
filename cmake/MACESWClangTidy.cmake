c4b0b56bd0f3245d881ee1af9553cf1eb4dee0b5
zhaoshihan <zhao_shihan@foxmail.com>
2025-10-31 12:17:59 +0800

    Fix clang-tidy with unity build

--- cmake/MACESWClangTidy.cmake
+++ cmake/MACESWClangTidy.cmake
@@ -8,11 +8,27 @@ if(NOT CMAKE_CXX_COMPILER_ID MATCHES "^(GNU|Clang)$")
     message(NOTICE "***Notice: Not using GCC or LLVM Clang. For the time turning off MACESW_CLANG_TIDY")
 endif()
 
+include(CMakePackageConfigHelpers)
+set(MACESW_CLANG_TIDY_FIX_OPTION_1 "- -readability-redundant-declaration")
+configure_package_config_file(${MACESW_PROJECT_TOOL_DIR}/clang-tidy.in
+                              ${MACESW_PROJECT_TOOL_DIR}/clang-tidy-fix-unity-build.yml
+                              INSTALL_DESTINATION ${MACESW_PROJECT_TOOL_DIR})
+set(MACESW_CLANG_TIDY_FIX_OPTION_1 "")
+configure_package_config_file(${MACESW_PROJECT_TOOL_DIR}/clang-tidy.in
+                              ${MACESW_PROJECT_ROOT_DIR}/.clang-tidy
+                              INSTALL_DESTINATION ${MACESW_PROJECT_ROOT_DIR})
+
 if(MACESW_CLANG_TIDY)
     set(MACESW_CLANG_TIDY_FULL_COMMAND
             ${CLANG_TIDY_EXE}
-                --config-file=${MACESW_PROJECT_ROOT_DIR}/.clang-tidy
                 --header-filter=${MACESW_PROJECT_SOURCE_DIR}/.*)
+    if(MACESW_UNITY_BUILD)
+        list(APPEND MACESW_CLANG_TIDY_FULL_COMMAND --config-file=${MACESW_PROJECT_TOOL_DIR}/clang-tidy-fix-unity-build.yml)
+    elseif(MACESW_CLANG_TIDY_AMEND_UNITY_BUILD)
+        list(APPEND MACESW_CLANG_TIDY_FULL_COMMAND --config-file=${MACESW_PROJECT_TOOL_DIR}/clang-tidy-amend-unity-build.yml)
+    else()
+        list(APPEND MACESW_CLANG_TIDY_FULL_COMMAND --config-file=${MACESW_PROJECT_ROOT_DIR}/.clang-tidy)
+    endif()
     if(MACESW_CLANG_FORMAT_WERROR)
         list(APPEND MACESW_CLANG_TIDY_FULL_COMMAND --warnings-as-errors=*)
     endif()
@@ -21,9 +37,6 @@ if(MACESW_CLANG_TIDY)
         message(STATUS "MACESW source code will be analysed by ${CLANG_TIDY_EXE} (warnings as errors)")
     else()
         message(STATUS "MACESW source code will be analysed by ${CLANG_TIDY_EXE}")
-    endif()
-    if(MACESW_UNITY_BUILD)
-        message(WARNING "clang-tidy may generate false positives when using unity build. Try disable unity build (MACESW_UNITY_BUILD=OFF) to perform code analysis")
     endif()
 else()
     message(NOTICE "***Notice: MACESW source code will not be analysed by clang-tidy")
